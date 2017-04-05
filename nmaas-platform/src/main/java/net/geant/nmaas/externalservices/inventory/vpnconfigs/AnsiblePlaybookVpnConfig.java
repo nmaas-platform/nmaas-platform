d93ef6617a649c743e1095ff0ada36181a3a7e17
Jakub Gutkowski <jgutkow@man.poznan.pl>
2017-04-05 16:05:50 +0200

    NMAAS-99: Added first version of input validation.

--- nmaas-platform/src/main/java/net/geant/nmaas/externalservices/inventory/vpnconfigs/AnsiblePlaybookVpnConfig.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/externalservices/inventory/vpnconfigs/AnsiblePlaybookVpnConfig.java
@@ -1,5 +1,8 @@
 package net.geant.nmaas.externalservices.inventory.vpnconfigs;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Ansible playbooks vpn configuration properties.
  *
@@ -7,6 +10,8 @@ package net.geant.nmaas.externalservices.inventory.vpnconfigs;
  */
 public class AnsiblePlaybookVpnConfig {
 
+    private Type type;
+    private static final int MAX_PROPRTY_LENGTH = 20;
     private String targetRouter;
     private String vrfId;
     private String logicalInterface;
@@ -24,6 +29,20 @@ public class AnsiblePlaybookVpnConfig {
     private String policyStatementConnected;
     private String policyStatementImport;
     private String policyStatementExport;
+
+    AnsiblePlaybookVpnConfig() {}
+
+    AnsiblePlaybookVpnConfig(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
 
     public String getTargetRouter() {
         return targetRouter;
@@ -168,6 +187,7 @@ public class AnsiblePlaybookVpnConfig {
 
         AnsiblePlaybookVpnConfig that = (AnsiblePlaybookVpnConfig) o;
 
+        if (type != that.type) return false;
         if (getTargetRouter() != null ? !getTargetRouter().equals(that.getTargetRouter()) : that.getTargetRouter() != null)
             return false;
         if (getVrfId() != null ? !getVrfId().equals(that.getVrfId()) : that.getVrfId() != null) return false;
@@ -201,7 +221,8 @@ public class AnsiblePlaybookVpnConfig {
 
     @Override
     public int hashCode() {
-        int result = getTargetRouter() != null ? getTargetRouter().hashCode() : 0;
+        int result = type.hashCode();
+        result = 31 * result + (getTargetRouter() != null ? getTargetRouter().hashCode() : 0);
         result = 31 * result + (getVrfId() != null ? getVrfId().hashCode() : 0);
         result = 31 * result + (getLogicalInterface() != null ? getLogicalInterface().hashCode() : 0);
         result = 31 * result + (getVrfRd() != null ? getVrfRd().hashCode() : 0);
@@ -219,5 +240,131 @@ public class AnsiblePlaybookVpnConfig {
         result = 31 * result + (getPolicyStatementImport() != null ? getPolicyStatementImport().hashCode() : 0);
         result = 31 * result + (getPolicyStatementExport() != null ? getPolicyStatementExport().hashCode() : 0);
         return result;
+    }
+
+    public void validate() throws AnsiblePlaybookVpnConfigInvalidException {
+        StringBuilder exceptionMessage = new StringBuilder();
+        if (targetRouter == null || targetRouter.isEmpty()) {
+            nullMessage("Target Router", exceptionMessage);
+        } else if(targetRouter.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("Target Router", exceptionMessage);
+        }
+        if (vrfId == null || vrfId.isEmpty()) {
+            nullMessage("VRF ID", exceptionMessage);
+        } else if (vrfId.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("VRF ID", exceptionMessage);
+        }
+        if (logicalInterface == null || logicalInterface.isEmpty()) {
+            nullMessage("Logical Interface", exceptionMessage);
+        } else if(logicalInterface.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("Logical Interface", exceptionMessage);
+        }
+        if (vrfRd == null || vrfRd.isEmpty()) {
+            nullMessage("VRF RD", exceptionMessage);
+        } else if (vrfRd.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("VRF RD", exceptionMessage);
+        }  else if (validateIpAddress(vrfRd)) {
+            wrongIpMessage("VRF RD: " +  vrfRd, exceptionMessage);
+        }
+        if (vrfRt == null || vrfRt.isEmpty()) {
+            nullMessage("VRF RT", exceptionMessage);
+        } else if (vrfRt.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("VRF RT", exceptionMessage);
+        }
+        if (bgpGroupId == null || bgpGroupId.isEmpty()) {
+            nullMessage("BGP Group ID", exceptionMessage);
+        } else if (bgpGroupId.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("BGP Group ID", exceptionMessage);
+        }
+        if (bgpNeighborIp == null || bgpNeighborIp.isEmpty()) {
+            nullMessage("BGP Neighbor ID", exceptionMessage);
+        } else if (bgpNeighborIp.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("BGP Neighbor ID", exceptionMessage);
+        }
+        if (asn == null || asn.isEmpty()) {
+            nullMessage("ASN", exceptionMessage);
+        } else if (asn.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("ASN", exceptionMessage);
+        }
+        if (physicalInterface == null || physicalInterface.isEmpty()) {
+            nullMessage("Physical Interface", exceptionMessage);
+        } else if (physicalInterface.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("Physical Interface", exceptionMessage);
+        }
+        if (interfaceUnit == null || interfaceUnit.isEmpty()) {
+            nullMessage("Interface Unit", exceptionMessage);
+        } else if (interfaceUnit.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("Interface Unit", exceptionMessage);
+        }
+        if (interfaceVlan == null || interfaceUnit.isEmpty()) {
+            nullMessage("Interface VLAN", exceptionMessage);
+        } else if (interfaceVlan.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("Interface VLAN", exceptionMessage);
+        }
+        if (bgpLocalIp == null || bgpLocalIp.isEmpty()) {
+            nullMessage("BGP Local IP", exceptionMessage);
+        }  else if (bgpLocalIp.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("BGP Local IP", exceptionMessage);
+        }
+        if (bgpLocalCidr == null || bgpLocalCidr.isEmpty()) {
+            nullMessage("BGP Local CIDR", exceptionMessage);
+        } else if (bgpLocalCidr.length() > MAX_PROPRTY_LENGTH) {
+            tooLongMessage("BGP Local CIDR", exceptionMessage);
+        }
+        if (type.equals(Type.CLOUD_SIDE)) {
+            if (policyCommunityOptions == null || policyCommunityOptions.isEmpty()) {
+                nullMessage("Policy Community Options", exceptionMessage);
+            } else if (policyCommunityOptions.length() > MAX_PROPRTY_LENGTH) {
+                tooLongMessage("Policy Community Options", exceptionMessage);
+            }
+            if (policyStatementConnected == null || policyStatementConnected.isEmpty()) {
+                nullMessage("Policy Statement Connected", exceptionMessage);
+            } else if (policyStatementConnected.length() > MAX_PROPRTY_LENGTH) {
+                tooLongMessage("Policy Statement Connected", exceptionMessage);
+            }
+            if (policyStatementImport == null || policyStatementImport.isEmpty()) {
+                nullMessage("Policy Statement Import", exceptionMessage);
+            } else if (policyStatementImport.length() > MAX_PROPRTY_LENGTH) {
+                tooLongMessage("Policy Statement Import", exceptionMessage);
+            }
+            if (policyStatementExport == null || policyStatementExport.isEmpty()) {
+                nullMessage("Policy Statement Export", exceptionMessage);
+            } else if (policyStatementExport.length() > MAX_PROPRTY_LENGTH) {
+                tooLongMessage("Policy Statement Export", exceptionMessage);
+            }
+        }
+        exception(exceptionMessage.toString());
+    }
+
+    private void wrongIpMessage(String fieldName, StringBuilder exceptionMessage) {
+        exceptionMessage.append(fieldName).append(" is not in proper format/n");
+    }
+
+    private boolean validateIpAddress(String ipAddress) {
+        try {
+            InetAddress.getByName(ipAddress);
+            return true;
+        } catch (UnknownHostException e) {
+            return false;
+        }
+    }
+
+    private void exception(String message) throws AnsiblePlaybookVpnConfigInvalidException {
+        if(message.length() > 0) {
+            throw new AnsiblePlaybookVpnConfigInvalidException(message);
+        }
+    }
+
+    private void nullMessage(String fieldName, StringBuilder exceptionMessage) {
+        exceptionMessage.append(fieldName).append(" is NULL or empty/n");
+    }
+
+    private void tooLongMessage(String fieldName, StringBuilder exceptionMessage) {
+        exceptionMessage.append(fieldName).append(" is too long/n");
+    }
+
+    public enum Type {
+        CLIENT_SIDE,
+        CLOUD_SIDE
     }
 }
