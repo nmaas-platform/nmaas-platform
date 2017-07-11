9ea2e860a115e67e1e29426f69f65b5d1f033757
Lukasz Lopatowski <llopat@man.poznan.pl>
2017-07-11 16:32:38 +0200

    NMAAS-164: Fixed inventory initialization script to use tokens.

--- nmaas-platform/src/test/shell/populate-inventory.sh
+++ nmaas-platform/src/test/shell/populate-inventory.sh
@@ -4,10 +4,10 @@ function getToken() {
 	python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)['token']))" | sed -e 's/^"//' -e 's/"$//'
 }
 
-API_URL=http://localhost:9000/portal/api
-echo $API_URL
+PORTAL_API_URL=http://localhost:9000/portal/api
+echo $PORTAL_API_URL
 
-TOKEN=`curl -sX POST $API_URL/auth/basic/login --header "Content-Type: application/json" --header "Accept: application/json" -d @data/login.json | getToken`
+TOKEN=`curl -sX POST $PORTAL_API_URL/auth/basic/login --header "Content-Type: application/json" --header "Accept: application/json" -d @data/login.json | getToken`
 
 
 echo Token:
@@ -15,7 +15,10 @@ echo ----------------------
 echo $TOKEN
 echo ----------------------
 echo Ping
-curl -X GET $API_URL/auth/basic/ping --header "Authorization: Bearer $TOKEN"
+curl -X GET $PORTAL_API_URL/auth/basic/ping --header "Authorization: Bearer $TOKEN"
+
+API_URL=http://localhost:9000/platform/api
+echo $API_URL
 
 echo
 echo Adding default Docker Hosts
