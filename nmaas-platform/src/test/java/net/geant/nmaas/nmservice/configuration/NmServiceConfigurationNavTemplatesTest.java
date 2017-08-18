c023c41648133110489139173effbd11df7b44b3
Lukasz Lopatowski <llopat@man.poznan.pl>
2017-08-18 14:17:54 +0200

    NMAAS-195: Add support for applications that do not have/require any configuration files

---
+++ nmaas-platform/src/test/java/net/geant/nmaas/nmservice/configuration/NmServiceConfigurationNavTemplatesTest.java
@@ -0,0 +1,54 @@
+package net.geant.nmaas.nmservice.configuration;
+
+import net.geant.nmaas.nmservice.configuration.repositories.NmServiceConfigurationTemplatesRepository;
+import net.geant.nmaas.orchestration.entities.AppConfiguration;
+import net.geant.nmaas.orchestration.entities.Identifier;
+import net.geant.nmaas.portal.persistent.entity.Application;
+import net.geant.nmaas.portal.persistent.repositories.ApplicationRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Lukasz Lopatowski <llopat@man.poznan.pl>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NmServiceConfigurationNavTemplatesTest {
+
+    @Autowired
+    private NmServiceConfigurationTemplatesRepository templatesRepository;
+
+    @Autowired
+    private NmServiceConfigurationsPreparer configurationsPreparer;
+
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
+    private Long navAppId;
+
+    @Before
+    public void setup() {
+        Application app = new Application("navAppName");
+        app.setVersion("navAppVersion");
+        navAppId = applicationRepository.save(app).getId();
+    }
+
+    @Test
+    public void shouldProceedNormallyForApplicationWithoutAnyConfigurationFiles() throws Exception {
+        configurationsPreparer.generateAndStoreConfigurations(
+                null,
+                Identifier.newInstance(String.valueOf(navAppId)),
+                new AppConfiguration("{}"));
+    }
+
+    @After
+    public void removeTestAppFromDatabase() {
+        applicationRepository.deleteAll();
+    }
+
+}
