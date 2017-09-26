8a32892fd6c4fba510c6b5792def1788a7bd0828
Michal Giertych <michal.giertych@man.poznan.pl>
2017-09-27 00:55:49 +0200

    NMAAS-205: comments and ratings added

--- nmaas-portal/src/app/service/apps.service.ts
+++ nmaas-portal/src/app/service/apps.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AuthHttp } from 'angular2-jwt';
 import { Http, Headers, Request, Response, RequestOptions, RequestOptionsArgs} from '@angular/http';
 
+import { Id } from '../model/id';
 import { Application } from '../model/application';
 import { Rate } from '../model/rate';
 import { Comment } from '../model/comment';
@@ -24,7 +25,7 @@ export class AppsService {
         return this.authHttp.get(this.appConfig.getApiUrl() + '/apps')
             .timeout(10000)
             .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
     }
 
     public getApp(id: Number): Observable<Application> {
@@ -32,28 +33,48 @@ export class AppsService {
         return this.authHttp.get(this.appConfig.getApiUrl() + '/apps/' + id)
             .timeout(10000)
             .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
     }
 
     public getAppRateByUrl(urlPath: string): Observable<Rate> {
         return this.authHttp.get(this.appConfig.getApiUrl() + urlPath)
             .timeout(10000)
             .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
+    }
+
+    public setMyAppRateByUrl(urlPath: string): Observable<any> {
+        return this.authHttp.post(this.appConfig.getApiUrl() + urlPath, null)
+            .timeout(10000)
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
     }
 
     public getAppCommentsByUrl(urlPath: string): Observable<Comment[]> {
         return this.authHttp.get(this.appConfig.getApiUrl() + urlPath)
             .timeout(10000)
             .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
     }
 
+    public addAppCommentByUrl(urlPath: string, comment: Comment): Observable<Id> {
+        return this.authHttp.post(this.appConfig.getApiUrl() + urlPath, comment)
+            .timeout(10000)
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
+    }
+
+    public deleteAppCommentByUrl(urlPath: string, id: Id): Observable<any> {
+        return this.authHttp.delete(this.appConfig.getApiUrl() + urlPath + '/' + id.id)
+            .timeout(10000)            
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
+    }
+    
     public getAppScreenshotsByUrl(urlPath: string): Observable<FileInfo[]> {
         return this.authHttp.get(this.appConfig.getApiUrl() + urlPath)
             .timeout(10000)
             .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+            .catch((error: any) => Observable.throw((typeof error.json === "function" ? error.json().message : 'Server error')));
     }
 
     public getAppScreenshotUrl(urlPath: string): string {
