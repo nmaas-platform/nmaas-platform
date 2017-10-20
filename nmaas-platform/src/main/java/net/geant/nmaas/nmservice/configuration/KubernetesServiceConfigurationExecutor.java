c6223d280a193d28edee147b1c5bd90b0c4b02ad
Lukasz Lopatowski <llopat@man.poznan.pl>
2017-10-20 13:24:50 +0200

    NMAAS-162: Added empty implementation of service configuration

--- nmaas-platform/src/main/java/net/geant/nmaas/nmservice/configuration/KubernetesServiceConfigurationExecutor.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/nmservice/configuration/KubernetesServiceConfigurationExecutor.java
@@ -1,8 +1,12 @@
 package net.geant.nmaas.nmservice.configuration;
 
+import net.geant.nmaas.nmservice.NmServiceDeploymentStateChangeEvent;
 import net.geant.nmaas.nmservice.configuration.exceptions.NmServiceConfigurationFailedException;
+import net.geant.nmaas.nmservice.deployment.entities.NmServiceDeploymentState;
 import net.geant.nmaas.orchestration.entities.AppConfiguration;
 import net.geant.nmaas.orchestration.entities.Identifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +17,17 @@ import org.springframework.stereotype.Component;
 @Profile("kubernetes")
 public class KubernetesServiceConfigurationExecutor implements NmServiceConfigurationProvider {
 
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
     @Override
     public void configureNmService(Identifier deploymentId, Identifier applicationId, AppConfiguration configuration) throws NmServiceConfigurationFailedException {
 
+        notifyStateChangeListeners(deploymentId, NmServiceDeploymentState.CONFIGURED);
+    }
+
+    private void notifyStateChangeListeners(Identifier deploymentId, NmServiceDeploymentState state) {
+        applicationEventPublisher.publishEvent(new NmServiceDeploymentStateChangeEvent(this, deploymentId, state));
     }
 
 }
