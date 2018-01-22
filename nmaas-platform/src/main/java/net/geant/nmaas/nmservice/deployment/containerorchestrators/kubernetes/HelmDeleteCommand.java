249c0ae20e819cb51107aa5ce1f9ec6865be4420
Lukasz Lopatowski <llopat@man.poznan.pl>
2018-01-22 14:55:00 +0100

    NMAAS-244: Added "purge" flag to helm delete command

--- nmaas-platform/src/main/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/HelmDeleteCommand.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/HelmDeleteCommand.java
@@ -8,6 +8,7 @@ import java.util.function.Predicate;
 public class HelmDeleteCommand extends HelmCommand {
 
     private static final String DELETE = "delete";
+    private static final String OPTION_PURGE = "--purge";
 
     /**
      * Creates {@link HelmDeleteCommand} with provided custom input.
@@ -19,7 +20,7 @@ public class HelmDeleteCommand extends HelmCommand {
         if (releaseName == null || releaseName.isEmpty())
             throw new IllegalArgumentException("Name of the release can't be null or empty");
         StringBuilder sb = new StringBuilder();
-        sb.append(HELM).append(SPACE).append(DELETE).append(SPACE).append(releaseName);
+        sb.append(HELM).append(SPACE).append(DELETE).append(SPACE).append(OPTION_PURGE).append(SPACE).append(releaseName);
         return new HelmDeleteCommand(sb.toString());
     }
 
