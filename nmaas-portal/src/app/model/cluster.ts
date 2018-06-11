29c770ad04a4768f8e9214b44dbc825243752935
Michał Bień <michal1.bien@gmail.com>
2018-06-11 12:35:35 +0200

    NMAAS-310: introduced frontend cluster API models

---
+++ nmaas-portal/src/app/model/cluster.ts
@@ -0,0 +1,67 @@
+import { HostAddress } from "./hostaddress";
+
+class ClusterInfo {
+    public helmHostAddress: HostAddress;
+    public name: String;
+    public restApiHostAddress: HostAddress;
+    public restApiPort: number;
+}
+
+class ClusterApi {
+    public id: number;
+    public restApiHostAddress: HostAddress;
+    public restApiPort: number;
+}
+
+class ClusterAttachPoint {
+    public id: number;
+    public routerId: string;
+    public routerInterfaceName: string;
+    public routerName: string;
+}
+
+class ClusterDeployment {
+    public defaultNamespace: string;
+    public defaultPersistenceClass: string;
+    public id: number;
+    public useDefaultNamespace: boolean;
+}
+
+class ClusterExtNetwork {
+    public assigned: boolean;
+    public assignedSince: string;
+    public assignedTo: string;
+    public externalIp: HostAddress;
+    public externalNetwork: HostAddress;
+    public externalNetworkMaskLength: number;
+    public id: number;
+}
+
+class ClusterHelm {
+    public helmHostAddress: HostAddress;
+    public helmHostChartsDirectory: string;
+    public helmHostSshUsername: string;
+    public id: number;
+    public useLocalChartArchives: boolean;
+}
+
+class ClusterIngress {
+    public controllerChartArchive: string;
+    public externalServiceDomain: string;
+    public id: number;
+    public supportedIngressClass: string;
+    public tlsSupported: boolean;
+    public useExistingController: boolean;
+    public useExistingIngress: boolean;
+}
+
+class Cluster {
+    public api: ClusterApi;
+    public attachPoint: ClusterAttachPoint;
+    public deployment: ClusterDeployment;
+    public externalNetworks: ClusterExtNetwork[];
+    public helm: ClusterHelm;
+    public id: number;
+    public ingress: ClusterIngress;
+    public name: string;
+}
---
+++ nmaas-portal/src/app/model/hostaddress.ts
@@ -0,0 +1,16 @@
+export class HostAddress {
+    public address: string;
+    public anyLocalAddress: boolean;
+    public canonicalHostName: string;
+    public hostAddress: string;
+    public hostName: string;
+    public linkLocalAddress: boolean;
+    public loopbackAddress: boolean;
+    public mcglobal: boolean;
+    public mclinkLocal: boolean;
+    public mcnodeLocal: boolean;
+    public mcorgLocal: boolean;
+    public mcsiteLocal: boolean;
+    public multicastAddress: boolean;
+    public siteLocalAddress: boolean;
+}
