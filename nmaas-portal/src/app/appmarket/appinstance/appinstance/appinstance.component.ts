cec6d54dfc0ba4a94a7ebdc221f47c6bf21d619a
Kacper Figurski <kfigurski@man.poznan.pl>
2018-07-19 14:38:20 +0200

    NMAAS-122 Password is now hidden in configuration form

--- nmaas-portal/src/app/appmarket/appinstance/appinstance/appinstance.component.ts
+++ nmaas-portal/src/app/appmarket/appinstance/appinstance/appinstance.component.ts
@@ -1,26 +1,24 @@
-import {Component, OnInit, OnDestroy, Input, ViewChild} from '@angular/core';
-import {Router, ActivatedRoute, Params} from '@angular/router';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Location} from '@angular/common';
 
 import {IntervalObservable} from 'rxjs/observable/IntervalObservable';
-// import 'rxjs/add/operator/switchMap';
-
-import {AppsService, AppInstanceService, AppImagesService} from '../../../service/index';
+import {AppImagesService, AppInstanceService, AppsService} from '../../../service/index';
 
 import {AppInstanceProgressComponent} from '../appinstanceprogress/appinstanceprogress.component';
-import {RateComponent} from '../../../shared/rate/rate.component';
 
 import {
-  Application,
-  AppInstance,
-  AppInstanceState,
-  AppInstanceStatus,
-  AppInstanceProgressStage
+    AppInstance,
+    AppInstanceProgressStage,
+    AppInstanceState,
+    AppInstanceStatus,
+    Application
 } from '../../../model/index';
 
 import {SecurePipe} from '../../../pipe/index';
-import { isNullOrUndefined } from 'util';
 import {AppRestartModalComponent} from "../../modals/apprestart";
+
+// import 'rxjs/add/operator/switchMap';
 
 
 @Component({
@@ -55,7 +53,7 @@ export class AppInstanceComponent implements OnInit, OnDestroy {
     loadExternalAssets: false, // Load external css and JavaScript for frameworks
     returnEmptyFields: false, // Don't return values for empty input fields
     setSchemaDefaults: true, // Always use schema defaults for empty fields
-    defautWidgetOptions: { feedback: false }, // Show inline feedback icons
+    defaultWidgetOptions: { feedback: false }, // Show inline feedback icons
     options: {},
     widgetOptions: {}
   };
@@ -78,7 +76,6 @@ export class AppInstanceComponent implements OnInit, OnDestroy {
           this.configurationTemplate = this.getTemplate(this.app.configTemplate.template);
         });
       });
-
 
       this.updateAppInstanceState();
       this.intervalCheckerSubscribtion = IntervalObservable.create(5000).subscribe(() => this.updateAppInstanceState());
