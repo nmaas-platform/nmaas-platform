0ccd1b40f73125616c4b90baa1915709dfb0b00d
Kacper Figurski <kfigurski@man.poznan.pl>
2018-08-06 19:15:31 +0200

    NMAAS-357 Test fixed

--- nmaas-platform/src/test/java/net/geant/nmaas/dcn/deployment/ManualDcnDeploymentExecutorWorkflowTest.java
+++ nmaas-platform/src/test/java/net/geant/nmaas/dcn/deployment/ManualDcnDeploymentExecutorWorkflowTest.java
@@ -4,6 +4,8 @@ import net.geant.nmaas.orchestration.AppDeploymentRepositoryManager;
 import net.geant.nmaas.orchestration.entities.Identifier;
 import net.geant.nmaas.orchestration.events.app.AppRequestNewOrVerifyExistingDcnEvent;
 import net.geant.nmaas.orchestration.exceptions.InvalidDomainException;
+import net.geant.nmaas.portal.persistent.entity.Domain;
+import net.geant.nmaas.portal.persistent.repositories.DomainRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +32,19 @@ public class ManualDcnDeploymentExecutorWorkflowTest {
     private AppDeploymentRepositoryManager appDeploymentRepositoryManager;
     @MockBean
     private DcnRepositoryManager dcnRepositoryManager;
+    @Autowired
+    private DomainRepository domainRepository;
 
     private static final Identifier DEPLOYMENT_ID = Identifier.newInstance("did");
     private static final String DOMAIN = "domain";
 
     @Test
     public void shouldCompleteDcnWorkflowWithManualExecutor() throws Exception {
+        Domain domain = new Domain(DOMAIN, DOMAIN, true, "namespace", true);
+        domainRepository.save(domain);
         when(appDeploymentRepositoryManager.loadDomainByDeploymentId(any())).thenReturn(DOMAIN);
         when(dcnRepositoryManager.loadCurrentState(DOMAIN)).thenThrow(new InvalidDomainException());
+        when(dcnRepositoryManager.exists(DOMAIN)).thenReturn(true);
         eventPublisher.publishEvent(new AppRequestNewOrVerifyExistingDcnEvent(this, DEPLOYMENT_ID));
         verify(appDeploymentRepositoryManager, timeout(500)).loadAllWaitingForDcn(DOMAIN);
     }
