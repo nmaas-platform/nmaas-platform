afa00e057b763c446d26a838aec34721121d9f51
Lukasz Lopatowski <llopat@man.poznan.pl>
2018-08-14 11:51:12 +0200

    NMAAS-346: Fixed dummy test method

--- nmaas-platform/src/test/java/net/geant/nmaas/orchestration/DefaultAppLifecycleManagerTest.java
+++ nmaas-platform/src/test/java/net/geant/nmaas/orchestration/DefaultAppLifecycleManagerTest.java
@@ -1,16 +1,19 @@
 package net.geant.nmaas.orchestration;
 
+import net.geant.nmaas.orchestration.entities.AppDeployment;
 import net.geant.nmaas.orchestration.entities.Identifier;
 import net.geant.nmaas.orchestration.repositories.AppDeploymentRepository;
 import net.geant.nmaas.portal.persistent.entity.Application;
 import net.geant.nmaas.portal.persistent.repositories.ApplicationRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -26,10 +29,12 @@ public class DefaultAppLifecycleManagerTest {
     @Autowired
     private DefaultAppLifecycleManager appLifecycleManager;
 
-    @Autowired
-    AppDeploymentRepository appDepRepository;
     @MockBean
     private ApplicationRepository appRepository;
+    @MockBean
+    AppDeploymentRepository appDepRepository;
+    @MockBean
+    AppDeploymentRepositoryManager appDepRepositoryManager;
 
     @Test
     public void shouldGenerateProperIdentifier() {
@@ -37,13 +42,14 @@ public class DefaultAppLifecycleManagerTest {
         assertThat(id.value().matches("[a-z]([-a-z0-9]*[a-z0-9])?"), is(true));
     }
 
-    @Transactional
     @Test
     public void shouldFailToDeployApplicationInstance() throws InterruptedException {
         when(appRepository.findOne(1L)).thenReturn(new Application("appName"));
+        when(appDepRepository.findByDeploymentId(Matchers.any())).thenReturn(Optional.of(
+                new AppDeployment(Identifier.newInstance("deploymentId"), "domain1", Identifier.newInstance(1L), "deploymentName")));
+        when(appDepRepositoryManager.load(Matchers.any())).thenReturn(Optional.empty());
         appLifecycleManager.deployApplication("domain1", Identifier.newInstance(1L), "deploymentName");
-        Thread.sleep(1000);
-        appDepRepository.deleteAll();
+        Thread.sleep(200);
     }
 
 }
