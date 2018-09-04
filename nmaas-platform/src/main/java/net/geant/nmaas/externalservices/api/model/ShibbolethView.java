7e4eeee776dcfa21a27d035170c3a04b170755a3
Kacper Figurski <kfigurski@man.poznan.pl>
2018-09-04 13:18:33 +0200

    NMAAS-448 Unnecessary constructor removed

--- nmaas-platform/src/main/java/net/geant/nmaas/externalservices/api/model/ShibbolethView.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/externalservices/api/model/ShibbolethView.java
@@ -16,12 +16,4 @@ public class ShibbolethView {
     private boolean allowsBasic;
     private String key;
     private int timeout;
-
-    public ShibbolethView(String loginUrl, String logoutUrl, boolean allowsBasic, String key, int timeout){
-        this.loginUrl = loginUrl;
-        this.logoutUrl = logoutUrl;
-        this.allowsBasic = allowsBasic;
-        this.key = key;
-        this.timeout = timeout;
-    }
 }
