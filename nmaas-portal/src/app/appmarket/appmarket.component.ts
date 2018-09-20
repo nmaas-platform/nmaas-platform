d3ce32361a06dcf37c2e9d8a93cbcb926acc9dc8
Remigiusz Gębka <rgebka@man.poznan.pl>
2018-09-20 23:13:44 +0200

    Footer update; update to landing page due to comments;

--- nmaas-portal/src/app/appmarket/appmarket.component.ts
+++ nmaas-portal/src/app/appmarket/appmarket.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {AfterViewChecked, Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-appmarket',
@@ -6,11 +6,28 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: [ '../../assets/css/main.css', './appmarket.component.css' ]
 //  encapsulation: ViewEncapsulation.None
 })
-export class AppMarketComponent implements OnInit {
+export class AppMarketComponent implements OnInit, AfterViewChecked {
+
+  private height = 0;
 
   constructor() { }
 
   ngOnInit() {
+      this.onResize();
   }
 
+    ngAfterViewChecked(){
+        this.onResize();
+    }
+
+    onResize() {
+        this.height = document.getElementById("global-footer").offsetHeight;
+        console.log(`Footer h: ${this.height}`);
+        document.getElementById("appmarket-container").style.marginBottom = `${this.height}px`;
+        if(this.height > 60){
+            document.getElementById("global-footer").style.textAlign = "center";
+        }else{
+            document.getElementById("global-footer").style.textAlign = "right";
+        }
+    }
 }
--- nmaas-portal/src/app/welcome/welcome.component.ts
+++ nmaas-portal/src/app/welcome/welcome.component.ts
@@ -1,12 +1,12 @@
 import { AppConfigService } from '../service/appconfig.service';
-import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, AfterViewChecked{
 
   private height = 0;
 
@@ -16,14 +16,20 @@ export class WelcomeComponent implements OnInit {
     this.onResize();
   }
 
+  ngAfterViewChecked(){
+      this.onResize();
+  }
+
   onResize() {
-      this.height = document.getElementById("global-footer").offsetHeight + 10;
-      //console.log(`Footer h: ${height}`);
-      document.getElementById("welcome-container").style.marginBottom = `${this.height}px`;
-      if(this.height > 75){
+      this.height = document.getElementById("global-footer").offsetHeight;
+      console.log(`Footer h: ${this.height}`);
+      document.getElementById("welcome-container").style.marginBottom = `${this.height + 15}px`;
+      if(this.height > 60){
         document.getElementById("global-footer").style.textAlign = "center";
+          document.getElementById("global-footer-version").style.lineHeight = `inherit`;
       }else{
         document.getElementById("global-footer").style.textAlign = "right";
+        document.getElementById("global-footer-version").style.lineHeight = `${this.height}px`;
       }
   }
 
