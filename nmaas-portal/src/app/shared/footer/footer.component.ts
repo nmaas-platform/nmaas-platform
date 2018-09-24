e17a9c4b43a3a123d1bbf2ef0548a6922aa51ecb
Remigiusz Gębka <rgebka@man.poznan.pl>
2018-09-24 11:49:24 +0200

    Changelog moved to modal (new component based on modal comp.)

--- nmaas-portal/src/app/shared/footer/footer.component.ts
+++ nmaas-portal/src/app/shared/footer/footer.component.ts
@@ -2,6 +2,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import {ChangelogService} from "../../service";
 import {ModalComponent} from "../modal";
+import {ModalChangelogComponent} from "./modal-changelog/modal-changelog.component";
 
 @Component({
   selector: 'nmaas-footer',
@@ -11,6 +12,9 @@ import {ModalComponent} from "../modal";
 export class FooterComponent implements OnInit {
 
   gitInfo:string[];
+
+  @ViewChild(ModalChangelogComponent)
+  private changeLog: ModalChangelogComponent;
 
   @ViewChild(ModalComponent)
   private modal:ModalComponent;
