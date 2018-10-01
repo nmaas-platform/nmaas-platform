1ea7397349d6f2ef42a4c1c11fff0da0909695c8
Kacper Figurski <kfigurski@man.poznan.pl>
2018-10-01 11:41:23 +0200

    NMAAS-503 Now templates are initialized while creating Application entity

--- nmaas-platform/src/main/java/net/geant/nmaas/portal/api/domain/AppDeploymentSpec.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/portal/api/domain/AppDeploymentSpec.java
@@ -2,6 +2,8 @@ package net.geant.nmaas.portal.api.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import net.geant.nmaas.nmservice.deployment.containerorchestrators.dockercompose.entities.DockerComposeFileTemplate;
+import net.geant.nmaas.nmservice.deployment.containerorchestrators.kubernetes.entities.KubernetesTemplate;
 import net.geant.nmaas.orchestration.entities.AppDeploymentEnv;
 
 import java.util.List;
@@ -13,6 +15,10 @@ public class AppDeploymentSpec {
     private Long id;
 
     private List<AppDeploymentEnv> supportedDeploymentEnvironments;
+
+    private KubernetesTemplate kubernetesTemplate;
+
+    private DockerComposeFileTemplate dockerComposeFileTemplate;
 
     private Integer defaultStorageSpace;
 
