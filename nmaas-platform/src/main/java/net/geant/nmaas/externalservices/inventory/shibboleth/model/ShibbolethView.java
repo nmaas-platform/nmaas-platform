a5b86ec956c3042701ef2e808ef7abfd3804c274
Michał Bień <mbien@man.poznan.pl>
2018-10-01 13:46:56 +0200

    Cleaned up unused annotation

--- nmaas-platform/src/main/java/net/geant/nmaas/externalservices/inventory/shibboleth/model/ShibbolethView.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/externalservices/inventory/shibboleth/model/ShibbolethView.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 @Setter
