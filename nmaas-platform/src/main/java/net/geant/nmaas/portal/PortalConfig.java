880bbdd6446df3a423ba743d62e834c441667f1e
Remigiusz Gębka <rgebka@man.poznan.pl>
2018-10-08 12:52:51 +0200

    PortalConfig optional check bugfix

--- nmaas-platform/src/main/java/net/geant/nmaas/portal/PortalConfig.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/portal/PortalConfig.java
@@ -2,6 +2,7 @@ package net.geant.nmaas.portal;
 
 import net.geant.nmaas.portal.exceptions.ProcessingException;
 import net.geant.nmaas.portal.persistent.entity.Content;
+import net.geant.nmaas.portal.persistent.entity.Domain;
 import net.geant.nmaas.portal.persistent.entity.Role;
 import net.geant.nmaas.portal.persistent.entity.User;
 import net.geant.nmaas.portal.persistent.repositories.ContentRepository;
@@ -62,9 +63,12 @@ public class PortalConfig {
 			}
 
 			private void addUser(String username, String password, String email, Role role) {
-				User user = new User(username, true, passwordEncoder.encode(password), domains.getGlobalDomain().get(), role, true, true);
-				user.setEmail(email);
-				userRepository.save(user);
+				Optional<Domain> globalDomain = domains.getGlobalDomain();
+				if(globalDomain.isPresent()) {
+					User user = new User(username, true, passwordEncoder.encode(password), globalDomain.get(), role, true, true);
+					user.setEmail(email);
+					userRepository.save(user);
+				}
 			}
 						
 		};
