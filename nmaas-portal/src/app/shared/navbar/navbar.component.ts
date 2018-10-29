0209fbf5e723f3cb37396819d9045afb38f3490b
Remigiusz Gębka <rgebka@man.poznan.pl>
2018-10-29 12:36:43 +0100

    landing page navbar update - dropdown for language change introduced;

--- nmaas-portal/src/app/shared/navbar/navbar.component.ts
+++ nmaas-portal/src/app/shared/navbar/navbar.component.ts
@@ -9,14 +9,27 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public languageActual = '';
+  public pathToIcon = '';
+
   constructor(private translate: TranslateService) {
     const browserLang = translate.currentLang == null ? 'en' : translate.currentLang;
     translate.use(browserLang.match(/en|fr|pl/) ? browserLang : 'en');
+    this.languageActual = this.getCurrent();
+    this.pathToIcon = 'assets/images/country/' + this.getCurrent() + '_circle.png';
   }
+
   useLanguage(language: string) {
     this.translate.use(language);
     this.translate.setDefaultLang(language);
+    this.languageActual = this.getCurrent();
+    this.pathToIcon = 'assets/images/country/' + language + '_circle.png';
   }
+
+  getCurrent(){
+    return this.translate.currentLang;
+  }
+
   ngOnInit() {
   }
 }
