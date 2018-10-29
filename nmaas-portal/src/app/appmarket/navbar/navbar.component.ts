db2c76b7401591f0f52901e222b8ea76600d173f
Remigiusz Gębka <rgebka@man.poznan.pl>
2018-10-29 13:11:07 +0100

    appmarket navbar update - dropdown for language change introduced; new dropdown is not at this moment mobile friendly

--- nmaas-portal/src/app/appmarket/navbar/navbar.component.ts
+++ nmaas-portal/src/app/appmarket/navbar/navbar.component.ts
@@ -14,17 +14,30 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class NavbarComponent implements OnInit {
 
+    public languageActual = '';
+    public pathToIcon = '';
+
   constructor(public authService: AuthService, public domainService: DomainService, private translate: TranslateService) {
     translate.addLangs(['en', 'fr', 'pl']);
     const browserLang = translate.currentLang == null ? 'en' : translate.currentLang;
     translate.use(browserLang.match(/en|fr|pl/) ? browserLang : 'en');
-  }
-  useLanguage(language: string) {
-    this.translate.use(language);
+    this.languageActual = this.getCurrent();
+    this.pathToIcon = 'assets/images/country/' + this.getCurrent() + '_circle.png';
   }
 
   ngOnInit() {
   }
+
+    useLanguage(language: string) {
+        this.translate.use(language);
+        this.translate.setDefaultLang(language);
+        this.languageActual = this.getCurrent();
+        this.pathToIcon = 'assets/images/country/' + language + '_circle.png';
+    }
+
+    getCurrent(){
+        return this.translate.currentLang;
+    }
 
   public checkUserRole(): boolean {
     return this.authService.getDomains().filter(value => value ! = this.domainService.getGlobalDomainId()).length > 0
