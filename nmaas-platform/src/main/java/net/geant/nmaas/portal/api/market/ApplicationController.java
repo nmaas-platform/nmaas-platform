d0a2e98b33a0a9429657249a7b9ef8f58daf8e7d
Sascha Schweiger <sascha.schweiger@fau.de>
2018-11-02 17:09:44 +0100

    Some changes

--- nmaas-platform/src/main/java/net/geant/nmaas/portal/api/market/ApplicationController.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/portal/api/market/ApplicationController.java
@@ -9,7 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,13 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/apps")
 public class ApplicationController extends AppBaseController {
 	
-	@RequestMapping(method= RequestMethod.GET)
+	@GetMapping
 	@Transactional
 	public List<ApplicationBrief> getApplications() {
 		return applications.findAll().stream().map(app -> modelMapper.map(app, ApplicationBrief.class)).collect(Collectors.toList());
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
+	@PostMapping
 	@PreAuthorize("hasRole('ROLE_SYSTEM_ADMIN') || hasRole('ROLE_TOOL_MANAGER')")
 	@Transactional
 	public Id addApplication(@RequestBody(required=true) Application appRequest) {
@@ -38,7 +41,7 @@ public class ApplicationController extends AppBaseController {
 	@RequestMapping(value="/{appId}", method=RequestMethod.GET)
 	@PreAuthorize("hasPermission(#appId, 'application', 'READ')")
 	@Transactional
-	public Application getApplication(@PathVariable(value = "appId", required=true) Long id) throws MissingElementException {
+	public Application getApplication(@PathVariable(value = "appId", required=true) Long id) {
 		net.geant.nmaas.portal.persistent.entity.Application app = getApp(id); 
 		return modelMapper.map(app, Application.class);
 	}
