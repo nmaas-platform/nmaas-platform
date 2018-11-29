6547509f879e94dd752870b2ed281b5e0cc51a51
Kacper Figurski <kfigurski@man.poznan.pl>
2018-11-29 10:33:47 +0100

    NMAAS-541 Small changes

--- nmaas-platform/src/main/java/net/geant/nmaas/nmservice/configuration/ConfigurationFileTransferProvider.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/nmservice/configuration/ConfigurationFileTransferProvider.java
@@ -24,6 +24,13 @@ public interface ConfigurationFileTransferProvider {
      */
     void transferConfigFiles(Identifier deploymentId, List<String> configIds, boolean configFileRepositoryRequired);
 
+    /**
+     * Method to transfer a list of updated configuration files
+     *
+     * @param deploymentId unique identifier of service deployment
+     * @param configIds list of identifiers of configuration files that should be loaded from database and transferred to destination
+     * @param configFileRepositoryRequired indicates if GitLab instance is required during deployment
+     */
     void updateConfigFiles(Identifier deploymentId, List<String> configIds, boolean configFileRepositoryRequired);
 
 }
