640155e0d374127bd844a1701d6196e2ee41c3ea
Remigiusz Gębka <rgebka@man.poznan.pl>
2018-12-11 09:55:24 +0100

    OpenNTI logo png -> svg

--- nmaas-platform/src/test/shell/init.bat
+++ nmaas-platform/src/test/shell/init.bat
@@ -142,7 +142,7 @@ echo App4
 curl -X POST %API_URL%/apps --header "Authorization: Bearer %token%" --header "Content-Type: application/json" --header "Accept: application/json" -d @data\apps\app4-opennti.json
 echo.
 echo App4 logo
-curl -X POST --header "Authorization: Bearer %token%" -F "file=@data\apps\images\logo\open_nti.png;type=image/png" %API_URL%/apps/4/logo
+curl -X POST --header "Authorization: Bearer %token%" -F "file=@data\apps\images\logo\open-nti.svg;type=image/svg+xml" %API_URL%/apps/4/logo
 echo.
 echo App4 screenshots
 curl -X POST --header "Authorization: Bearer %token%" -F "file=@data\apps\images\screenshots\opennti\opennti1.png;type=image/jpg" %API_URL%/apps/4/screenshots
