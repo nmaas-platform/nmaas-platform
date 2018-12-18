c0f71467a9f416875a32cd2ba75db2c21b767e6e
Kacper Figurski <kfigurski@man.poznan.pl>
2018-12-18 16:44:15 +0100

    Minor improvements

--- nmaas-portal/src/app/appmarket/applist/applist.component.spec.ts
+++ nmaas-portal/src/app/appmarket/applist/applist.component.spec.ts
@@ -1,15 +1,23 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { AppListComponent } from './applist.component';
+import { AppListComponent as AppSharedListComponent} from "../../shared/applications/list/applist.component";
 import {RouterTestingModule} from "@angular/router/testing";
 import {AppSubscriptionsService} from "../../service/appsubscriptions.service";
 import {UserDataService} from "../../service/userdata.service";
 import {ApplicationsViewComponent} from "../../shared/applications/applications.component";
-import {AppConfigService, AppsService} from "../../service";
+import {AppConfigService, AppsService, TagService} from "../../service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {SearchComponent} from "../../shared/common/search/search.component";
+import {TagFilterComponent} from "../../shared/common/tagfilter/tagfilter.component";
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AppElementComponent} from "../../shared/applications/list/element/appelement.component";
+import {RateComponent} from "../../shared/rate";
+import {PipesModule} from "../../pipe/pipes.module";
+import {AppInstallModalComponent} from "../../shared/modal/appinstall";
+import {ModalComponent} from "../../shared/modal";
+import {Observable} from "rxjs";
 
 describe('ApplistComponent', () => {
   let component: AppListComponent;
@@ -17,19 +25,27 @@ describe('ApplistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppListComponent ],
+      declarations: [ AppListComponent, ApplicationsViewComponent, SearchComponent, AppInstallModalComponent, ModalComponent,
+          AppSharedListComponent, TagFilterComponent, AppElementComponent, RateComponent],
         imports:[
             RouterTestingModule,
+            PipesModule,
             HttpClientTestingModule,
-
+            TranslateModule.forRoot({
+                loader: {
+                    provide: TranslateLoader,
+                    useClass: TranslateFakeLoader
+                }
+            }),
+            FormsModule,
+            ReactiveFormsModule
         ],
         providers: [
             AppConfigService,
-            ApplicationsViewComponent,
             AppSubscriptionsService,
             UserDataService,
             AppsService,
-
+            TagService
         ]
     })
     .compileComponents();
@@ -38,7 +54,13 @@ describe('ApplistComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppListComponent);
     component = fixture.componentInstance;
+    spyOn(fixture.debugElement.injector.get(TagService), 'getTags').and.returnValue(Observable.of([]));
     fixture.detectChanges();
+  });
+
+  it('should create component', () => {
+      let app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
   });
 
 });
