de463dce10c3671546498f4b57eff8efe6c7b3eb
Lukasz Lopatowski <llopat@man.poznan.pl>
2019-01-10 16:30:37 +0100

    NMAAS-590: Fixing migration

--- nmaas-platform/src/main/java/net/geant/nmaas/externalservices/inventory/kubernetes/model/KClusterView.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/externalservices/inventory/kubernetes/model/KClusterView.java
@@ -3,8 +3,6 @@ package net.geant.nmaas.externalservices.inventory.kubernetes.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.net.InetAddress;
-
 @Getter
 @Setter
 public class KClusterView {
--- nmaas-platform/src/main/resources/db/migration/V1.1.0_20190110_1405__removedKubernetesApi.sql
+++ nmaas-platform/src/main/resources/db/migration/V1.1.0_20190110_1405__removedKubernetesApi.sql
@@ -1,3 +1,3 @@
-drop table k_cluster_api;
-
 alter table k_cluster drop column api_id;
+
+drop table k_cluster_api;
