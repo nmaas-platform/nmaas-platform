dc9cb7478c0c5607f52bccdeb0a96d96fa5bd243
Lukasz Lopatowski <llopat@man.poznan.pl>
2019-01-11 11:16:28 +0100

    NMAAS-590: Some sonar fixes

--- nmaas-platform/src/main/java/net/geant/nmaas/portal/ApiSecurityConfig.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/portal/ApiSecurityConfig.java
@@ -1,13 +1,6 @@
 package net.geant.nmaas.portal;
 
 import net.geant.nmaas.portal.api.security.ApiPermissionEvaluator;
-import net.geant.nmaas.portal.service.impl.security.AclService;
-import net.geant.nmaas.portal.service.impl.security.AppInstancePermissionCheck;
-import net.geant.nmaas.portal.service.impl.security.AppTemplatePermissionCheck;
-import net.geant.nmaas.portal.service.impl.security.CommentPermissionCheck;
-import net.geant.nmaas.portal.service.impl.security.DomainObjectPermissionCheck;
-import net.geant.nmaas.portal.service.impl.security.GenericPermissionCheck;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
