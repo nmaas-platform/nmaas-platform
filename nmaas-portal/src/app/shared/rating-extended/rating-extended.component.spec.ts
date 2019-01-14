4b1f92f29d640348706442e0671a990d81c04a86
Remigiusz Gębka <rgebka@man.poznan.pl>
2019-01-14 11:37:52 +0100

    Tests fix: added translation pipe mock

--- nmaas-portal/src/app/shared/rating-extended/rating-extended.component.spec.ts
+++ nmaas-portal/src/app/shared/rating-extended/rating-extended.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 
 import { RatingExtendedComponent } from './rating-extended.component';
 import {AppsService} from "../../service";
@@ -6,6 +6,7 @@ import {Observable} from "rxjs";
 import {Rate} from "../../model";
 import {HttpResponse} from "selenium-webdriver/http";
 import {TooltipModule} from "ng2-tooltip-directive";
+import {Pipe, PipeTransform} from "@angular/core";
 
 class MockAppService{
   public getAppRateByUrl(urlPath: string): Observable<Rate> {
@@ -17,18 +18,30 @@ class MockAppService{
   }
 }
 
+@Pipe({
+  name: "translate"
+})
+export class TranslatePipeMock implements PipeTransform {
+  public name: string = "translate";
+
+  public transform(query: string, ...args: any[]): any {
+    return query;
+  }
+}
+
 describe('RatingExtendedComponent', () => {
   let component: RatingExtendedComponent;
   let fixture: ComponentFixture<RatingExtendedComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RatingExtendedComponent ],
+      declarations: [ RatingExtendedComponent, TranslatePipeMock ],
       imports: [TooltipModule],
-      providers: [ {provide: AppsService, useClass: MockAppService}]
-    })
-    .compileComponents();
-
+      providers: [
+        {provide: AppsService, useClass: MockAppService},
+      ]
+    });
+  TestBed.compileComponents();
   }));
 
   beforeEach(() => {
