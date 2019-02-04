cdaf797de361960e4a3e9eb56763514876286db3
Michał Bień <mbien@man.poznan.pl>
2019-02-04 13:03:47 +0100

    Implemented basicauth proto platform-side functions

--- nmaas-platform/src/main/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/components/janitor/JanitorService.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/components/janitor/JanitorService.java
@@ -2,6 +2,9 @@ package net.geant.nmaas.nmservice.deployment.containerorchestrators.kubernetes.c
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import net.geant.nmaas.externalservices.inventory.janitor.BasicAuthServiceGrpc;
+import net.geant.nmaas.externalservices.inventory.janitor.ConfigServiceGrpc;
+import net.geant.nmaas.externalservices.inventory.janitor.JanitorManager;
 import net.geant.nmaas.externalservices.inventory.kubernetes.KNamespaceService;
 import net.geant.nmaas.orchestration.entities.Identifier;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,52 +21,93 @@ public class JanitorService {
 
     private KNamespaceService namespaceService;
 
+    private ManagedChannel channel;
+
     @Autowired
     public JanitorService(KNamespaceService namespaceService) {
         this.namespaceService = namespaceService;
+        this.channel = ManagedChannelBuilder.forAddress(janitorHost, Integer.parseInt(janitorPort)).usePlaintext().build();
     }
 
-    private JanitorManager.ConfigUpdateRequest buildRequest(Identifier deploymentId, String domain) {
+    private JanitorManager.InstanceRequest buildInstanceRequest(Identifier deploymentId, String domain) {
+
         JanitorManager.Instance instance = JanitorManager.Instance.newBuilder().
                 setNamespace(namespaceService.namespace(domain)).
                 setUid(deploymentId.value()).
                 setDomain(domain).
                 build();
-        return JanitorManager.ConfigUpdateRequest.newBuilder().
+        return JanitorManager.InstanceRequest.newBuilder().
                 setApi("v1").
                 setDeployment(instance).
                 build();
     }
 
+    private JanitorManager.InstanceCredentialsRequest buildInstanceCredentialsRequest(Identifier deploymentId, String domain, String user, String password) {
+        JanitorManager.Instance instance = JanitorManager.Instance.newBuilder().
+                setNamespace(namespaceService.namespace(domain)).
+                setUid(deploymentId.value()).
+                setDomain(domain).
+                build();
+        JanitorManager.Credentials credentials = JanitorManager.Credentials.newBuilder().
+                setUser(user).
+                setPassword(password).
+                build();
+
+        return JanitorManager.InstanceCredentialsRequest.newBuilder().
+                setApi("v1").
+                setInstance(instance).
+                setCredentials(credentials).
+                build();
+    }
+
     public void createConfigMap(Identifier deploymentId, String domain) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(janitorHost, Integer.parseInt(janitorPort)).usePlaintext().build();
         ConfigServiceGrpc.ConfigServiceBlockingStub stub = ConfigServiceGrpc.newBlockingStub(channel);
 
-        JanitorManager.ConfigUpdateResponse response = stub.create(buildRequest(deploymentId, domain));
-        channel.shutdown();
+        JanitorManager.ServiceResponse response = stub.create(buildInstanceRequest(deploymentId, domain));
 
-        if(response.getStatus() != JanitorManager.Status.OK)
+        if (response.getStatus() != JanitorManager.Status.OK)
             throw new ConfigMapCreationException(response.getMessage());
     }
 
     public void updateConfigMap(Identifier deploymentId, String domain) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(janitorHost, Integer.parseInt(janitorPort)).usePlaintext().build();
         ConfigServiceGrpc.ConfigServiceBlockingStub stub = ConfigServiceGrpc.newBlockingStub(channel);
 
-        JanitorManager.ConfigUpdateResponse response = stub.update(buildRequest(deploymentId, domain));
-        channel.shutdown();
+        JanitorManager.ServiceResponse response = stub.update(buildInstanceRequest(deploymentId, domain));
 
-        if(response.getStatus() != JanitorManager.Status.OK)
+        if (response.getStatus() != JanitorManager.Status.OK)
             throw new ConfigMapCreationException(response.getMessage());
     }
 
     public void deleteConfigMap(Identifier deploymentId, String domain) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(janitorHost, Integer.parseInt(janitorPort)).usePlaintext().build();
         ConfigServiceGrpc.ConfigServiceBlockingStub stub = ConfigServiceGrpc.newBlockingStub(channel);
 
-        JanitorManager.ConfigUpdateResponse response = stub.delete(buildRequest(deploymentId, domain));
-        channel.shutdown();
+        JanitorManager.ServiceResponse response = stub.delete(buildInstanceRequest(deploymentId, domain));
 
-        if(response.getStatus() != JanitorManager.Status.OK)
+        if (response.getStatus() != JanitorManager.Status.OK)
             throw new ConfigMapCreationException(response.getMessage());
-    }}
+    }
+
+    public void createBasicAuth(Identifier deploymentId, String domain, String user, String password) {
+        BasicAuthServiceGrpc.BasicAuthServiceBlockingStub stub = BasicAuthServiceGrpc.newBlockingStub(channel);
+
+        JanitorManager.ServiceResponse response = stub.create(buildInstanceCredentialsRequest(deploymentId, domain, user, password));
+        if (response.getStatus() != JanitorManager.Status.OK)
+            throw new ConfigMapCreationException(response.getMessage());
+    }
+
+    public void updateBasicAuth(Identifier deploymentId, String domain, String user, String password) {
+        BasicAuthServiceGrpc.BasicAuthServiceBlockingStub stub = BasicAuthServiceGrpc.newBlockingStub(channel);
+
+        JanitorManager.ServiceResponse response = stub.update(buildInstanceCredentialsRequest(deploymentId, domain, user, password));
+        if (response.getStatus() != JanitorManager.Status.OK)
+            throw new ConfigMapCreationException(response.getMessage());
+    }
+
+    public void deleteBasicAuth(Identifier deploymentId, String domain) {
+        BasicAuthServiceGrpc.BasicAuthServiceBlockingStub stub = BasicAuthServiceGrpc.newBlockingStub(channel);
+
+        JanitorManager.ServiceResponse response = stub.delete(buildInstanceRequest(deploymentId, domain));
+        if (response.getStatus() != JanitorManager.Status.OK)
+            throw new ConfigMapCreationException(response.getMessage());
+    }
+}
