b0ea191eeb242eea1e4ffabbc0d8d7df4e635595
Kacper Figurski <kfigurski@man.poznan.pl>
2019-02-12 10:19:47 +0100

    NMAAS-297 Missing models added

--- nmaas-portal/src/app/model/appdeploymentspec.ts
+++ nmaas-portal/src/app/model/appdeploymentspec.ts
@@ -1,4 +1,12 @@
+import {AppDeploymentEnv} from "./appdeploymentenv";
+import {ParameterType} from "./parametertype";
+import {KubernetesTemplate} from "./kubernetestemplate";
+
 export class AppDeploymentSpec {
     public id: number;
+    public supportedDeploymentEnvironments: AppDeploymentEnv[] = [AppDeploymentEnv.KUBERNETES];
+    public kubernetesTemplate: KubernetesTemplate;
     public defaultStorageSpace: number;
+    public deployParameters: Map<ParameterType, string>;
+    public configFileRepositoryRequired: boolean;
 }
---
+++ nmaas-portal/src/app/model/kubernetestemplate.ts
@@ -0,0 +1,6 @@
+import {KubernetesChart} from "./kuberneteschart";
+
+export class KubernetesTemplate{
+    public chart: KubernetesChart;
+    public archive: string;
+}
