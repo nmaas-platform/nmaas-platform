a7b1fdc68a4a07dc9d6ae1550ff07daf7c3beb92
Kacper Figurski <kfigurski@man.poznan.pl>
2019-02-28 16:10:43 +0100

    NMAAS-297 Migration fixed

--- nmaas-platform/src/main/resources/db/migration/V1.2.0_20190215_1010__applicationStateAddeds.sql
+++ nmaas-platform/src/main/resources/db/migration/V1.2.0_20190215_1010__applicationStateAddeds.sql
@@ -1,4 +1,4 @@
 alter table application drop column deleted;
 alter table application add column state varchar(255) not null default 'NEW';
 alter table application add column owner varchar(255) not null default 'admin';
-alter table application alter column version varchar(255) not null;
+alter table application alter column version set not null;
