3dce1e8f291a92e69ad3779c103ed7d8766314d2
Remigiusz Gębka <rgebka@man.poznan.pl>
2019-02-28 15:05:11 +0100

    Comments update

--- nmaas-portal/src/app/shared/comments/comments.component.ts
+++ nmaas-portal/src/app/shared/comments/comments.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation} from '@angular/core';
 
 import {AppsService} from '../../service/index';
 import {Comment, Id} from '../../model/index';
 import {isNullOrUndefined} from "util";
 import {AuthService} from "../../auth/auth.service";
 import {TranslateService} from '@ngx-translate/core';
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
     selector: 'comments',
@@ -13,7 +14,7 @@ import {TranslateService} from '@ngx-translate/core';
     encapsulation: ViewEncapsulation.None,
     providers: [AppsService]
 })
-export class CommentsComponent implements OnInit {
+export class CommentsComponent implements OnInit, OnChanges {
 
     @Input()
     private pathUrl: string;
@@ -34,23 +35,64 @@ export class CommentsComponent implements OnInit {
 
     commentErrorMsg: string;
 
-    constructor(private appsService: AppsService, private authService: AuthService, private translate: TranslateService) {
+    show: number[] = [];
+
+    showMainCommentsList = 5;
+
+    constructor(private appsService: AppsService, private authService: AuthService,
+                private translate: TranslateService) {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        //this.refresh();
     }
 
     ngOnInit() {
         this.refresh();
     }
 
+    public loadMoreComments(total: number, id: string){
+        document.getElementById(id).hidden = true;
+        if(this.showMainCommentsList + 5 >= total){
+            this.showMainCommentsList = total;
+        }else{
+            this.showMainCommentsList = this.showMainCommentsList + 5;
+        }
+    }
+
+    public loadMore(total: number, id: string, parent: number): void{
+        document.getElementById(id).hidden = true;
+        if(this.show[parent] + 3 >= total){
+            this.show[parent] = total;
+        }else{
+            this.show[parent] = this.show[parent] + 3;
+        }
+    }
+
     public refresh(): void {
-        this.appsService.getAppCommentsByUrl(this.pathUrl).subscribe(comments => this.comments = comments);
+        this.appsService.getAppCommentsByUrl(this.pathUrl).subscribe(comments =>{
+            this.comments = comments;
+            console.debug("Debug_comments: ", comments.length)
+            for(let comm of comments){
+                if(this.show[comm.id] == null || isNullOrUndefined(this.show[comm.id])) {
+                    this.show[comm.id] = 3;
+                }
+            }
+        });
+    }
+
+    public debugLog(i: number): void{
+        console.debug("Iterator i: ", i);
     }
 
     public addComment(): void {
+        console.debug("Add init");
         if(isNullOrUndefined(this.newComment.comment) || this.newComment.comment === ''){
             this.commentErrorMsg = this.translate.instant('SUBSCRIPTION.COMMENTS_NOT_EMPTY_MESSAGE');
         } else{
             this.appsService.addAppCommentByUrl(this.pathUrl, this.newComment)
                 .subscribe(id => {
+                    console.debug("Add successfully: ", this.newComment.comment, " ", this.newComment.parentId);
                     this.newComment = new Comment();
                     this.commentErrorMsg = undefined;
                     this.refresh();
