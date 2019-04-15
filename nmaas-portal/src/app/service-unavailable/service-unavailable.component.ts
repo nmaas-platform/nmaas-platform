9927a5b83a2da8f5fcf17384fa8058517620cc08
Remigiusz Gębka <rgebka@man.poznan.pl>
2019-04-15 10:51:08 +0200

    Service unavailable added

---
+++ nmaas-portal/src/app/service-unavailable/service-unavailable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-unavailable',
+  templateUrl: './service-unavailable.component.html',
+  styleUrls: ['./service-unavailable.component.css']
+})
+export class ServiceUnavailableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ nmaas-portal/src/app/service-unavailable/service-unavailable.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServiceUnavailableComponent } from './service-unavailable.component';
+
+@NgModule({
+  declarations: [ServiceUnavailableComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ServiceUnavailableModule { }
