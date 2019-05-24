8a9448f87b2bc0df1782cf11aba8c8d0cbd20de0
Remigiusz Gębka <rgebka@man.poznan.pl>
2019-05-24 11:08:15 +0200

    Temporary pp-subpage build test is disabled

--- nmaas-platform/src/main/java/net/geant/nmaas/notifications/MailAttributes.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/notifications/MailAttributes.java
@@ -2,6 +2,7 @@ package net.geant.nmaas.notifications;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import net.geant.nmaas.notifications.templates.MailType;
 
@@ -15,6 +16,7 @@ import net.geant.nmaas.portal.api.domain.UserView;
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
 public class MailAttributes implements Serializable {
 
     private List<UserView> addressees;
