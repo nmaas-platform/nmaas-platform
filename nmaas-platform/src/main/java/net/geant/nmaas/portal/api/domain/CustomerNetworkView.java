4b831e97e86f698ba85c13d88b1a4a59bb507465
Kacper Figurski <kfigurski@man.poznan.pl>
2019-08-21 13:01:03 +0200

    NMAAS-697 Minor improvement

--- nmaas-platform/src/main/java/net/geant/nmaas/portal/api/domain/CustomerNetworkView.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/portal/api/domain/CustomerNetworkView.java
@@ -1,13 +1,12 @@
 package net.geant.nmaas.portal.api.domain;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import java.net.InetAddress;
-import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -15,10 +14,4 @@ public class CustomerNetworkView {
     private Long id;
     private InetAddress customerIp;
     private int maskLength;
-    public CustomerNetworkView(Long id, InetAddress customerIp, int maskLength){
-        checkArgument(maskLength < 0 || maskLength > 24, "Invalid mask");
-        this.id = id;
-        this.customerIp = Objects.requireNonNull(customerIp, "IP address must be specified");
-        this.maskLength = maskLength;
-    }
 }
