e03858cbf70093414986c4131a9f61b2209e6e70
Wojciech Taisner <wtaisner@man.poznan.pl>
2020-01-09 08:21:11 +0100

    NMAAS-744 test data migration

--- nmaas-platform/src/main/resources/db/migration/common/V1.3.0_20200107_0824__createServiceAccessMethodTable.sql
+++ nmaas-platform/src/main/resources/db/migration/common/V1.3.0_20200107_0824__createServiceAccessMethodTable.sql
@@ -9,3 +9,6 @@ alter table kubernetes_nm_service_info_access_methods
     add constraint FKj2eolmo5vws87eicirpfvvkd
         foreign key (kubernetes_nm_service_info_id)
             references kubernetes_nm_service_info;
+
+insert into kubernetes_nm_service_info_access_methods
+select id, 'Default', 'DEFAULT', service_external_url from nmaas.public.kubernetes_nm_service_info;
