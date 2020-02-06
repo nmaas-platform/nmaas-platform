26a93a394e73dfdfea40471ff0a58f5f55efe390
Wojciech Taisner <wtaisner@man.poznan.pl>
2020-02-06 14:41:14 +0100

    NMAAS-765 trying to reproduce portal test build issue

--- nmaas-portal/src/app/appmarket/appinstance/appinstanceprogress/appinstanceprogress.component.spec.ts
+++ nmaas-portal/src/app/appmarket/appinstance/appinstanceprogress/appinstanceprogress.component.spec.ts
@@ -1,11 +1,10 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import {DebugElement, Pipe, PipeTransform} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Pipe, PipeTransform} from '@angular/core';
 
-import { AppInstanceProgressComponent } from './appinstanceprogress.component';
-import {AppsService} from "../../../service";
+import {AppInstanceProgressComponent} from './appinstanceprogress.component';
 import {TranslateService} from "@ngx-translate/core";
+import {AppInstanceProgressStage, AppInstanceState} from "../../../model";
 
 @Pipe({
   name: "translate"
@@ -46,5 +45,21 @@ describe('AppInstanceProgressComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('default stage list should be length 0', () => {
+    expect(component.stages.length).toEqual(0);
+  });
+
+  it('translate tag function should return', () => {
+    expect(component.getTranslateTag("some text")).toEqual('');
+  });
+
+  it('should display with given stages', () => {
+    let stages = new Array<AppInstanceProgressStage>();
+    stages.push(new AppInstanceProgressStage("Stage1", AppInstanceState.RUNNING));
+    component.stages = stages;
+    expect(component.stages.length).toEqual(1);
+    // TODO finish
   });
 });
