079f182b374c926dee273578cfa7c28172061ea4
Wojciech Taisner <wtaisner@man.poznan.pl>
2020-02-06 10:16:33 +0100

    NMAAS-765 more smells fixes and unit tests

--- nmaas-platform/src/test/java/net/geant/nmaas/portal/api/market/AppInstanceControllerTest.java
+++ nmaas-platform/src/test/java/net/geant/nmaas/portal/api/market/AppInstanceControllerTest.java
@@ -16,6 +16,7 @@ import net.geant.nmaas.portal.api.domain.AppInstanceView;
 import net.geant.nmaas.portal.persistent.entity.*;
 import net.geant.nmaas.portal.service.ApplicationInstanceService;
 import net.geant.nmaas.portal.service.DomainService;
+import net.geant.nmaas.portal.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
@@ -23,8 +24,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.security.Principal;
+import java.util.*;
 
 
 public class AppInstanceControllerTest {
@@ -34,23 +35,41 @@ public class AppInstanceControllerTest {
     private DomainService domainService = mock(DomainService.class);
     private ApplicationInstanceService applicationInstanceService = mock(ApplicationInstanceService.class);
     private AppDeploymentRepositoryManager appDeploymentRepositoryManager = mock(AppDeploymentRepositoryManager.class);
+    private UserService userService = mock(UserService.class);
 
     private AppInstanceController appInstanceController;
 
     private Application application;
     private Domain domain;
+    private Domain global;
     private String name = "app1";
     private String templateString = "{\"template\":\"xD\"}";
     private String identifierValue = "id12";
     private User owner;
+    private User admin;
 
     private Pageable pageable = mock(Pageable.class);
 
     @BeforeEach
     public void setup() {
-        owner = new User("admin");
-        domain = new Domain("domain one", "dom-1");
+        owner = new User("owner");
+        owner.setId(2L);
+        admin = new User("admin");
+        admin.setId(1L);
+        domain = new Domain(2L, "domain one", "dom-1");
+        global = new Domain(1L, "GLOBAL", "GLOBAL");
         application = new Application(name,"1.0","admin");
+        Set<UserRole> roleSet = new HashSet<>();
+        roleSet.add(new UserRole(admin, global, Role.ROLE_SYSTEM_ADMIN));
+        admin.setNewRoles(roleSet);
+
+        when(userService.findByUsername(admin.getUsername())).thenReturn(Optional.of(admin));
+        when(userService.findByUsername(owner.getUsername())).thenReturn(Optional.of(owner));
+        when(userService.findById(admin.getId())).thenReturn(Optional.of(admin));
+        when(userService.findById(owner.getId())).thenReturn(Optional.of(owner));
+
+        when(domainService.findDomain(global.getId())).thenReturn(Optional.of(global));
+        when(domainService.findDomain(domain.getId())).thenReturn(Optional.of(domain));
 
         ConfigWizardTemplate configWizardTemplate = mock(ConfigWizardTemplate.class);
         when(configWizardTemplate.getTemplate()).thenReturn(templateString);
@@ -63,15 +82,17 @@ public class AppInstanceControllerTest {
                 domainService,
                 appDeploymentRepositoryManager);
         appInstanceController.modelMapper = new ModelMapper();
+        appInstanceController.users = userService;
 
         when(pageable.getOffset()).thenReturn(0L);
         when(pageable.getPageNumber()).thenReturn(0);
         when(pageable.getPageSize()).thenReturn(20);
         when(pageable.getSort()).thenReturn(null);
+        when(domainService.getGlobalDomain()).thenReturn(Optional.of(global));
     }
 
     @Test
-    public void shouldReturnAllInstances() {
+    public void shouldGetAllInstancesWithPageable() {
         AppInstance appInstance = new AppInstance(application, name, domain, owner);
         List<AppInstance> appInstanceList = new ArrayList<>();
         appInstanceList.add(appInstance);
@@ -94,12 +115,52 @@ public class AppInstanceControllerTest {
     }
 
     @Test
-    public void shouldGetAllMyInstances() {
+    public void shouldGetAllInstancesWithParamsWhenIsSystemAdminAndDomainIsGlobal() {
+        AppInstance appInstance = new AppInstance(application, name, domain, admin);
+        List<AppInstance> appInstanceList = new ArrayList<>();
+        appInstanceList.add(appInstance);
+        Page<AppInstance> appInstancePage = new PageImpl<>(appInstanceList);
 
+        when(applicationInstanceService.findAll(pageable)).thenReturn(appInstancePage);
+        when(appDeploymentMonitor.userAccessDetails(any())).thenThrow(new InvalidDeploymentIdException());
+        AppDeployment appDeployment = mock(AppDeployment.class);
+        when(appDeployment.getDescriptiveDeploymentId()).thenReturn(new Identifier(identifierValue));
+        when(appDeploymentRepositoryManager.load(any())).thenReturn(appDeployment);
+
+        Principal principal = mock(Principal.class);
+        when(principal.getName()).thenReturn(admin.getUsername());
+
+        List<AppInstanceView> result = appInstanceController.getAllInstances(global.getId(), principal, pageable);
+
+        assertEquals(1, result.size());
+        AppInstanceView appInstanceView = result.get(0);
+        assertEquals(name, appInstanceView.getApplicationName());
+        assertEquals(admin.getUsername(), appInstanceView.getOwner().getUsername());
+        assertEquals(identifierValue, appInstanceView.getDescriptiveDeploymentId());
     }
 
     @Test
-    public void shouldGetUserDomainAppInstances() {
+    public void shouldGetAllMyInstancesInAllDomainWhenIsSystemAdminAndDomainIsGlobal() {
+        AppInstance appInstance = new AppInstance(application, name, domain, admin);
+        List<AppInstance> appInstanceList = new ArrayList<>();
+        appInstanceList.add(appInstance);
+        Page<AppInstance> appInstancePage = new PageImpl<>(appInstanceList);
 
+        when(applicationInstanceService.findAllByOwner(admin, pageable)).thenReturn(appInstancePage);
+        when(appDeploymentMonitor.userAccessDetails(any())).thenThrow(new InvalidDeploymentIdException());
+        AppDeployment appDeployment = mock(AppDeployment.class);
+        when(appDeployment.getDescriptiveDeploymentId()).thenReturn(new Identifier(identifierValue));
+        when(appDeploymentRepositoryManager.load(any())).thenReturn(appDeployment);
+
+        Principal principal = mock(Principal.class);
+        when(principal.getName()).thenReturn(admin.getUsername());
+
+        List<AppInstanceView> result = appInstanceController.getMyAllInstances(global.getId(), principal, pageable);
+
+        assertEquals(1, result.size());
+        AppInstanceView appInstanceView = result.get(0);
+        assertEquals(name, appInstanceView.getApplicationName());
+        assertEquals(admin.getUsername(), appInstanceView.getOwner().getUsername());
+        assertEquals(identifierValue, appInstanceView.getDescriptiveDeploymentId());
     }
 }
---
+++ nmaas-platform/src/test/java/net/geant/nmaas/portal/service/impl/security/AclServiceTest.java
@@ -0,0 +1,91 @@
+package net.geant.nmaas.portal.service.impl.security;
+
+import net.geant.nmaas.portal.persistent.entity.Domain;
+import net.geant.nmaas.portal.persistent.entity.Role;
+import net.geant.nmaas.portal.persistent.entity.User;
+import net.geant.nmaas.portal.persistent.entity.UserRole;
+import net.geant.nmaas.portal.service.AclService;
+import net.geant.nmaas.portal.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AclServiceTest {
+
+    private UserService userService = mock(UserService.class);
+
+    private AclServiceImpl service = null;
+
+    private DomainObjectPermissionCheck domain = mock(DomainObjectPermissionCheck.class);
+    private CommentPermissionCheck comment = mock(CommentPermissionCheck.class);
+    private AppInstancePermissionCheck instance = mock(AppInstancePermissionCheck.class);
+    private AppTemplatePermissionCheck template = mock(AppTemplatePermissionCheck.class);
+
+    @BeforeEach
+    public void setup() {
+
+        User user = new User("user");
+        Domain d = new Domain("domain-one", "code-1");
+        UserRole userRole = new UserRole(user, d, Role.ROLE_GUEST);
+        Set<UserRole> roleSet = new HashSet<>();
+        roleSet.add(userRole);
+        user.setNewRoles(roleSet);
+        when(userService.findById(-1L)).thenReturn(Optional.empty());
+        when(userService.findById(1L)).thenReturn(Optional.of(user));
+
+        service = new AclServiceImpl(domain, comment, instance, template, userService);
+    }
+
+    @Test
+    public void shouldAuthorizeAccessWithTemplatePermissionCheck() {
+        String targetType = "template";
+        when(domain.supports(targetType)).thenReturn(false);
+        when(comment.supports(targetType)).thenReturn(false);
+        when(instance.supports(targetType)).thenReturn(false);
+        when(template.supports(targetType)).thenReturn(true);
+
+        when(template.check(any(), any(), any(), (AclService.Permissions[]) any())).thenReturn(true);
+
+        assertTrue(service.isAuthorized(1L, "targetID", targetType, new AclService.Permissions[0]));
+    }
+
+    @Test
+    public void shouldNotAuthorizeWhenUserNotFound() {
+        assertFalse(service.isAuthorized(-1L, "targetID", "any", new AclService.Permissions[0]));
+    }
+
+    @Test
+    public void shouldNotAuthorizeWhenNoPermissionChecksNoPermissions() {
+        String targetType = "template";
+        when(domain.supports(targetType)).thenReturn(false);
+        when(comment.supports(targetType)).thenReturn(false);
+        when(instance.supports(targetType)).thenReturn(false);
+        when(template.supports(targetType)).thenReturn(false);
+
+        assertFalse(service.isAuthorized(1L, "targetID", targetType, new AclService.Permissions[0]));
+    }
+
+    @Test
+    public void shouldAuthorizeWhenNoPermissionChecksAndMatchingPermission() {
+        String targetType = "template";
+        when(domain.supports(targetType)).thenReturn(false);
+        when(comment.supports(targetType)).thenReturn(false);
+        when(instance.supports(targetType)).thenReturn(false);
+        when(template.supports(targetType)).thenReturn(false);
+
+        AclService.Permissions[] perms = new AclService.Permissions[1];
+        perms[0] = AclService.Permissions.READ;
+
+        assertTrue(service.isAuthorized(1L, "targetID", targetType, perms));
+    }
+
+}
