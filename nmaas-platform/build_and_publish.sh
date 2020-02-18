aa7da8909b203bace5868238a57636bb63719610
Lukasz Lopatowski <llopat@man.poznan.pl>
2020-02-18 15:37:10 +0100

    Update version files for 1.2.4

--- nmaas-platform/build_and_publish.sh
+++ nmaas-platform/build_and_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=1.2.4
+TAG=1.2.5
 PACKAGE=nmaas-platform
 REPOSITORY=artifactory.geant.net/nmaas-docker-local
 sudo docker build --rm -t $REPOSITORY/$PACKAGE:$TAG -f ./Dockerfile ..
--- nmaas-portal/build_and_publish.sh
+++ nmaas-portal/build_and_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=1.2.3
+TAG=1.2.4
 PACKAGE=nmaas-portal
 REPOSITORY=artifactory.geant.net/nmaas-docker-local
 sudo docker build --rm -t $REPOSITORY/$PACKAGE:$TAG -f ./Dockerfile ..
