90751d11165826e13c70d185491dae5fe2714d2d
wojtas1411 <wojtas.t.1997@gmail.com>
2020-03-31 09:42:36 +0200

    NMAAS-781 applied fixes and improvements

--- nmaas-portal/src/app/shared/users/list/userslist.component.ts
+++ nmaas-portal/src/app/shared/users/list/userslist.component.ts
@@ -9,10 +9,10 @@ import {Observable, of} from 'rxjs';
 
 import { isUndefined } from 'util';
 import {UserRole} from '../../../model/userrole';
-import {UserDataService} from "../../../service/userdata.service";
-import {AuthService} from "../../../auth/auth.service";
+import {UserDataService} from '../../../service/userdata.service';
+import {AuthService} from '../../../auth/auth.service';
 import {map, shareReplay, take} from 'rxjs/operators';
-import {CustomerSearchCriteria} from "../../../service";
+import {CustomerSearchCriteria} from '../../../service';
 
 @Component({
   selector: 'nmaas-userslist',
@@ -21,7 +21,7 @@ import {CustomerSearchCriteria} from "../../../service";
 })
 export class UsersListComponent extends BaseComponent implements OnInit, OnChanges {
 
-  public users_item_number_key = "NUMBER_OF_USERS_ITEM_KEY";
+  public users_item_number_key = 'NUMBER_OF_USERS_ITEM_KEY';
 
   @Input()
   public users: User[] = [];
@@ -40,8 +40,8 @@ export class UsersListComponent extends BaseComponent implements OnInit, OnChang
 
   public pageNumber = 1;
   public paginatorName = 'paginator-identifier';
-  public itemsPerPage: number[]  = [5, 10, 15, 20, 25, 30];
-  public maxItemsOnPage: number = 5;
+  public itemsPerPage: number[]  = [15, 20, 25, 30, 50 ];
+  public maxItemsOnPage = 15;
 
   constructor(private userService: UserService,
               public domainService: DomainService,
@@ -53,8 +53,8 @@ export class UsersListComponent extends BaseComponent implements OnInit, OnChang
 
   ngOnInit() {
     // set stored value of maxElementsPerPage
-    let i = sessionStorage.getItem(this.users_item_number_key);
-    if(i) this.maxItemsOnPage = +i;
+    const i = sessionStorage.getItem(this.users_item_number_key);
+    if (i) { this.maxItemsOnPage = +i; }
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -71,16 +71,16 @@ export class UsersListComponent extends BaseComponent implements OnInit, OnChang
     }
   }
 
-  public filterDomainNames(user: User): UserRole[]{
-    return user.roles.filter(role => role.domainId != this.domainService.getGlobalDomainId());
+  public filterDomainNames(user: User): UserRole[] {
+    return user.roles.filter(role => role.domainId !== this.domainService.getGlobalDomainId());
   }
 
-  public getOnlyDomainRoles(user: User): UserRole[]{
-    return user.roles.filter(role => role.domainId===this.domainId);
+  public getOnlyDomainRoles(user: User): UserRole[] {
+    return user.roles.filter(role => role.domainId === this.domainId);
   }
 
-  public getGlobalRole(user: User): string{
-    let userRole: UserRole[] = user.roles.filter(role => role.domainId === this.domainService.getGlobalDomainId());
+  public getGlobalRole(user: User): string {
+    const userRole: UserRole[] = user.roles.filter(role => role.domainId === this.domainService.getGlobalDomainId());
     return userRole[0].role.toString();
   }
 
@@ -112,7 +112,7 @@ export class UsersListComponent extends BaseComponent implements OnInit, OnChang
     this.userService.changeUserStatus(user.id, enabled).subscribe();
     user.enabled = enabled;
     // sort after changing params
-    if(this.lastSearchCriteria) this.handleSortEvent(this.lastSearchCriteria);
+    if (this.lastSearchCriteria) { this.handleSortEvent(this.lastSearchCriteria); }
   }
 
   onSorted($event) {
@@ -123,8 +123,8 @@ export class UsersListComponent extends BaseComponent implements OnInit, OnChang
   handleSortEvent(criteria: CustomerSearchCriteria) {
     this.lastSearchCriteria = criteria;
     const baseSortFunc = (a: any, b: any): number => {
-      if(a < b) return -1;
-      if(a > b) return  1;
+      if (a < b) { return -1; }
+      if (a > b) { return  1; }
       return 0;
     };
 
@@ -134,26 +134,23 @@ export class UsersListComponent extends BaseComponent implements OnInit, OnChang
           let result = 0;
 
           // sorting rules for custom columns
-          if(criteria.sortColumn === 'domains') {
+          if (criteria.sortColumn === 'domains') {
             const ad = this.filterDomainNames(a);
             const bd = this.filterDomainNames(b);
-            if (!ad) console.log(ad);
-            if (!bd) console.log(bd);
+            if (!ad) { console.log(ad); }
+            if (!bd) { console.log(bd); }
             const ar = ad.length > 0 ? ad[0].domainId : 0;
             const br = bd.length > 0 ? bd[0].domainId : 0;
             result = baseSortFunc(ar, br);
-          }
-          else if (criteria.sortColumn === 'globalRole') {
+          } else if (criteria.sortColumn === 'globalRole') {
             result = baseSortFunc(this.getGlobalRole(a) , this.getGlobalRole(b))
-          }
-          else if (criteria.sortColumn === 'roles') {
+          } else if (criteria.sortColumn === 'roles') {
             const ad = this.getOnlyDomainRoles(a);
             const bd = this.getOnlyDomainRoles(b);
             const ar = ad.length > 0 ? ad[0].role.toString() : '';
             const br = bd.length > 0 ? bd[0].role.toString() : '';
             result = baseSortFunc(ar, br);
-          } // default sorting rule
-          else {
+          } else {
             result = baseSortFunc(a[criteria.sortColumn], b[criteria.sortColumn]);
           }
 
@@ -162,7 +159,7 @@ export class UsersListComponent extends BaseComponent implements OnInit, OnChang
     )
   }
 
-  public setItems(item){
+  public setItems(item) {
     // store max items per page value in this session
     sessionStorage.setItem(this.users_item_number_key, item);
     this.maxItemsOnPage = item;
