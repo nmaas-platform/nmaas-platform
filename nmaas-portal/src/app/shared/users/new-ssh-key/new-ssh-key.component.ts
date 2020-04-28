f30f43a6dda6783b046624e1e70ff9066aed40e4
wojtas1411 <wojtas.t.1997@gmail.com>
2020-04-28 11:25:34 +0200

    NMAAS-804 frontend done

--- nmaas-portal/src/app/shared/users/new-ssh-key/new-ssh-key.component.ts
+++ nmaas-portal/src/app/shared/users/new-ssh-key/new-ssh-key.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {SSHKeyService} from '../../../service/sshkey.service';
 import {SSHKeyRequest} from '../../../model/sshkey-request';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ModalComponent} from '../../modal';
 
 @Component({
   selector: 'app-new-ssh-key',
@@ -10,6 +11,12 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class NewSshKeyComponent implements OnInit {
 
+  @ViewChild(ModalComponent)
+  public readonly modal: ModalComponent;
+
+  @Output()
+  public out: EventEmitter<any> = new EventEmitter<any>();
+
   public error: String = undefined;
 
   public requestForm: FormGroup = undefined;
@@ -17,20 +24,39 @@ export class NewSshKeyComponent implements OnInit {
   constructor(private keyService: SSHKeyService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.modal.setModalType('info');
+    this.modal.setStatusOfIcons(true);
+
     this.requestForm = this.formBuilder.group({
-      name: ['', Validators.required, Validators.minLength(6), Validators.maxLength(16)],
-      key: ['', Validators.required, Validators.pattern('^(ssh-rsa AAAAB3NzaC1yc2|ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNT|ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzOD|ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1Mj|ssh-ed25519 AAAAC3NzaC1lZDI1NTE5|ssh-dss AAAAB3NzaC1kc3)[0-9A-Za-z+/]+[=]{0,3}( .*)?$')],
+      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]],
+      key: ['', [Validators.required, Validators.pattern('^(ssh-rsa AAAAB3NzaC1yc2|ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNT|ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzOD|ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1Mj|ssh-ed25519 AAAAC3NzaC1lZDI1NTE5|ssh-dss AAAAB3NzaC1kc3)[0-9A-Za-z+/]+[=]{0,3}( .*)?$')]],
     })
   }
 
   public create() {
     const request = new SSHKeyRequest();
+    console.log(this.requestForm);
     request.name = this.requestForm.value.name;
     request.key = this.requestForm.value.key;
     this.keyService.createKey(request).subscribe(
-        data => {},
-        error => {}
+        data => {
+          this.error = undefined;
+          this.requestForm.reset();
+          this.modal.hide();
+          this.out.emit();
+        },
+        error => {
+          this.error = error.message;
+        }
     );
+  }
+
+  get name() {
+    return this.requestForm.get('name');
+  }
+
+  get key() {
+    return this.requestForm.get('key');
   }
 
 }
--- nmaas-portal/src/app/shared/users/ssh-keys/ssh-keys.component.ts
+++ nmaas-portal/src/app/shared/users/ssh-keys/ssh-keys.component.ts
@@ -11,11 +11,37 @@ import {SSHKeyView} from '../../../model/sshkey-view';
 export class SshKeysComponent implements OnInit {
 
   public keys: Observable<SSHKeyView[]> = undefined;
+  public keysList: SSHKeyView[] = [];
 
   constructor(private keyService: SSHKeyService) { }
 
   ngOnInit() {
     this.keys = this.keyService.getAll();
+    this.getData();
+  }
+
+  getData() {
+      this.keysList = [];
+      this.keys.subscribe(
+          data => {
+              this.keysList.push(...data);
+          },
+          error => {
+              console.error(error);
+          }
+      )
+  }
+
+  invalidate(id: number) {
+      this.keyService.invalidate(id).subscribe(
+          data => {
+              console.log('invalidating ssh key id: ' + id + ' success');
+              this.getData();
+          },
+          error => {
+              console.error(error);
+          }
+      );
   }
 
 }
