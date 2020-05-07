b31d04cd2d8611334f147abc8e600190231c6bef
Lukasz Lopatowski <llopat@man.poznan.pl>
2020-05-07 12:27:05 +0200

    NMAAS-808 Exposing webhooks endpoint without auth (temporary)

--- nmaas-platform/src/main/java/net/geant/nmaas/nmservice/configuration/api/GitLabWebhookController.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/nmservice/configuration/api/GitLabWebhookController.java
@@ -1,17 +1,20 @@
 package net.geant.nmaas.nmservice.configuration.api;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequestMapping("/api/gitlab/webhooks")
 public class GitLabWebhookController {
 
     @PostMapping("/{id}")
-    public void triggerWebhook(@PathVariable Long id) {
+    public void triggerWebhook(@PathVariable String id) {
         // TODO complete webhook handling
+        log.info("Triggered webhook with id: " + id);
     }
 
 }
