e9fe579b214e34bd5864b30368bcb998c5fd1100
wojtas1411 <wojtas.t.1997@gmail.com>
2020-05-26 14:55:48 +0200

    NMAAS-826 added component for creating/editing static deployment parameters

---
+++ nmaas-portal/src/app/appmarket/appmanagement/app-static-global-deploy-parameters-edit/app-static-global-deploy-parameters-edit.component.spec.ts
@@ -0,0 +1,49 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppStaticGlobalDeployParametersEditComponent } from './app-static-global-deploy-parameters-edit.component';
+import {FormsModule} from '@angular/forms';
+import {TooltipModule} from 'ng2-tooltip-directive';
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {AppStorageVolume} from '../../../model/app-storage-volume';
+import {AppAccessMethod} from '../../../model/app-access-method';
+
+describe('AppStaticGlobalDeployParametersEditComponent', () => {
+  let component: AppStaticGlobalDeployParametersEditComponent;
+  let fixture: ComponentFixture<AppStaticGlobalDeployParametersEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppStaticGlobalDeployParametersEditComponent ],
+      imports: [
+        FormsModule,
+        TooltipModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        })
+      ]
+    })
+        .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppStaticGlobalDeployParametersEditComponent);
+    component = fixture.componentInstance;
+    component.appDeploymentSpec = {
+      supportedDeploymentEnvironments: [],
+      kubernetesTemplate: undefined,
+      exposesWebUI: false,
+      deployParameters: {},
+      storageVolumes: [],
+      accessMethods:  [],
+      globalDeployParameters: {}
+    }
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- nmaas-portal/src/app/appmarket/appmanagement/app-storage-volume-edit-component/app-storage-volume-edit.component.ts
+++ nmaas-portal/src/app/appmarket/appmanagement/app-storage-volume-edit-component/app-storage-volume-edit.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {AppStorageVolume} from "../../../model/app-storage-volume";
-import {ServiceStorageVolume, ServiceStorageVolumeType} from "../../../model/servicestoragevolume";
+import {AppStorageVolume} from '../../../model/app-storage-volume';
+import {ServiceStorageVolume, ServiceStorageVolumeType} from '../../../model/servicestoragevolume';
 
 @Component({
   selector: 'app-storage-volume-edit',
@@ -30,20 +30,14 @@ export class AppStorageVolumeEditComponent implements OnInit {
 
   public defaultTooltipOptions = {
     'placement': 'bottom',
-    'show-delay': "50",
+    'show-delay': '50',
     'theme': 'dark'
   };
 
-  constructor() { }
-
-  ngOnInit() {
-    this.deployParamsMap = AppStorageVolumeEditComponent.convertObjectToStringMap(this.storageVolume.deployParameters)
-  }
-
   private static convertObjectToStringMap(arg: object): Map<string, string> {
-    let result = new Map<string, string>();
-    for(let k of Object.keys(arg)) {
-      if(!result.has(k) && typeof arg[k] === 'string') {
+    const result = new Map<string, string>();
+    for (const k of Object.keys(arg)) {
+      if (!result.has(k) && typeof arg[k] === 'string') {
         result.set(k, arg[k]);
       }
     }
@@ -52,24 +46,30 @@ export class AppStorageVolumeEditComponent implements OnInit {
 
   private static convertStringMapToObject(arg: Map<string, string>): object {
     const result = {};
-    for(let k of arg.keys()) {
+    for (const k of arg.keys()) {
       result[k] = arg.get(k);
     }
     return result;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+    this.deployParamsMap = AppStorageVolumeEditComponent.convertObjectToStringMap(this.storageVolume.deployParameters)
   }
 
   public isNewDeployParamValid(): boolean {
     if (!this.newKey || !this.newValue) {
       return false;
     }
-    if(!!this.newKey && this.deployParamsMap.has(this.newKey)) {
+    if (!!this.newKey && this.deployParamsMap.has(this.newKey)) {
       return false;
     }
     return (!!this.newKey && !!this.newValue);
   }
 
   public addNewDeployParam(): void {
-    if (this.isNewDeployParamValid()){
+    if (this.isNewDeployParamValid()) {
       this.deployParamsMap.set(this.newKey, this.newValue);
       this.newKey = '';
       this.newValue = '';
