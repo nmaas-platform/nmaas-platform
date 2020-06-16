185672ee5db94913de96df7ce121aa0d52841d6b
wojtas1411 <wojtas.t.1997@gmail.com>
2020-06-16 11:05:55 +0200

    NMAAS-835 add entity id frontend

--- nmaas-portal/src/app/model/appconfigurationspec.ts
+++ nmaas-portal/src/app/model/appconfigurationspec.ts
@@ -1,6 +1,7 @@
 import {ConfigFileTemplate} from './configfiletemplate';
 
 export class AppConfigurationSpec {
+    public id: number = undefined;
     public configFileRepositoryRequired = false;
     public configUpdateEnabled = false;
     public templates: ConfigFileTemplate[] = [];
