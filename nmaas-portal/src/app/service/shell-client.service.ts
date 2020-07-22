946203ec6f222b075e8096a7fe9e6081f05a048e
wojtas1411 <wojtas.t.1997@gmail.com>
2020-07-22 15:29:19 +0200

    NMAAS-791 apply further fixes and enhancements

--- nmaas-portal/src/app/service/shell-client.service.ts
+++ nmaas-portal/src/app/service/shell-client.service.ts
@@ -7,64 +7,67 @@ import {EventSourcePolyfill, OnMessageEvent} from 'ng-event-source';
 import {PodInfo} from '../model/podinfo';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ShellClientService {
 
-  private events: EventSourcePolyfill = undefined;
+    private events: EventSourcePolyfill = undefined;
 
-  constructor(private _zone: NgZone, private _sseService: SSEService, private http: HttpClient, private appConfig: AppConfigService) { }
+    constructor(private _zone: NgZone, private _sseService: SSEService, private http: HttpClient, private appConfig: AppConfigService) {
+    }
 
-  public initConnection(id: number, pod: string): Observable<string> {
-    // @ts-ignore
-    return this.http.post<string>(this.appConfig.getApiUrl() + '/shell/' + id + '/init/' + pod, {}, {responseType: 'text'});
-  }
+    public initConnection(id: number, pod: string): Observable<string> {
+        // @ts-ignore
+        return this.http.post<string>(this.appConfig.getApiUrl() + '/shell/' + id + '/init/' + pod, {}, {responseType: 'text'});
+    }
 
-  public sendCommand(sessionId: string, command: Object = {}): Observable<any> {
-    return this.http.post(this.appConfig.getApiUrl() + '/shell/' + sessionId + '/command', command);
-  }
+    public sendCommand(sessionId: string, command: Object = {}): Observable<any> {
+        return this.http.post(this.appConfig.getApiUrl() + '/shell/' + sessionId + '/command', command);
+    }
 
-  /**
-   * closes event stream, sends request to complete the session
-   */
-  public closeConnection(sessionId: string) {
-    this.closeEventStream()
-    this.http.delete(this.appConfig.getApiUrl() + '/shell/' + sessionId)
-  }
+    /**
+     * closes event stream, sends request to complete the session
+     */
+    public closeConnection(sessionId: string) {
+        this.closeEventStream()
+        this.http.delete(this.appConfig.getApiUrl() + '/shell/' + sessionId).subscribe(
+            () => console.log('session completed: ', sessionId),
+            error => console.error('error completing session', error))
+    }
 
-  public closeEventStream() {
-    this.events.close();
-    this.events = undefined;
-  }
+    public closeEventStream() {
+        this.events.close();
+        this.events = undefined;
+    }
 
-  public getServerSentEvent(sessionId: string): Observable<OnMessageEvent> {
-    return new Observable<OnMessageEvent>(observableEvents => {
-      const events = this._sseService.getEventSource(this.appConfig.getApiUrl() + '/shell/' + sessionId);
+    public getServerSentEvent(sessionId: string): Observable<OnMessageEvent> {
+        return new Observable<OnMessageEvent>(observableEvents => {
+            const events = this._sseService.getEventSource(this.appConfig.getApiUrl() + '/shell/' + sessionId);
 
-      events.onopen = onopenEvent => {
-        this._zone.run(() => {
-          observableEvents.next(onopenEvent);
-        })
-      };
+            events.onopen = onopenEvent => {
+                this._zone.run(() => {
+                    observableEvents.next(onopenEvent);
+                })
+            };
 
-      events.onmessage = onmessageEvent => {
-        this._zone.run(() => {
-          observableEvents.next(onmessageEvent);
-        })
-      };
+            events.onmessage = onmessageEvent => {
+                this._zone.run(() => {
+                    observableEvents.next(onmessageEvent);
+                })
+            };
 
-      events.onerror = onerrorEvent => {
-        this._zone.run(() => {
-          observableEvents.error(onerrorEvent);
-        })
-      };
+            events.onerror = onerrorEvent => {
+                this._zone.run(() => {
+                    observableEvents.error(onerrorEvent);
+                })
+            };
 
-      this.events = events;
-    });
-  }
+            this.events = events;
+        });
+    }
 
-  public getPossiblePods(id: number): Observable<PodInfo[]> {
-    return this.http.get<PodInfo[]>(this.appConfig.getApiUrl() + '/shell/' + id + '/podnames');
-  }
+    public getPossiblePods(id: number): Observable<PodInfo[]> {
+        return this.http.get<PodInfo[]>(this.appConfig.getApiUrl() + '/shell/' + id + '/podnames');
+    }
 
 }
