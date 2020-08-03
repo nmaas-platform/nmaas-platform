1cbd1d90871eec5c46dc38e83884b9705f4735a0
Wojciech Taisner <wtaisner@man.poznan.pl>
2020-08-03 11:03:08 +0200

    NMAAS-868 add filtering and sorting map function over observable

--- nmaas-portal/src/app/appmarket/appmanagement/appmanagementlist/appmanagementlist.component.ts
+++ nmaas-portal/src/app/appmarket/appmanagement/appmanagementlist/appmanagementlist.component.ts
@@ -9,54 +9,63 @@ import {ApplicationVersion} from "../../../model/applicationversion";
 import {map} from "rxjs/operators";
 
 @Component({
-  selector: 'nmaas-appmanagementlist',
-  templateUrl: './appmanagementlist.component.html',
-  styleUrls: ['./appmanagementlist.component.css']
+    selector: 'nmaas-appmanagementlist',
+    templateUrl: './appmanagementlist.component.html',
+    styleUrls: ['./appmanagementlist.component.css']
 })
 export class AppManagementListComponent implements OnInit {
 
-  @ViewChild(AppChangeStateModalComponent)
-  public modal: AppChangeStateModalComponent;
+    @ViewChild(AppChangeStateModalComponent)
+    public modal: AppChangeStateModalComponent;
 
-  public selectedAppName: string = "";
-  public selectedVersion: ApplicationVersion = new ApplicationVersion();
+    public selectedAppName: string = "";
+    public selectedVersion: ApplicationVersion = new ApplicationVersion();
 
-  public apps:Application[] = [];
+    public apps: Application[] = [];
 
-  constructor(public appsService:AppsService, public router:Router, public authService: AuthService) { }
+    constructor(public appsService: AppsService, public router: Router, public authService: AuthService) {
+    }
 
-  ngOnInit() {
-    this.appsService.getAllApps().pipe(
-        map(apps => {
-          const tmp = apps.filter(app => app.state !== ApplicationState.DELETED && ApplicationState[app.state] !== 'DELETED')
-          return tmp.sort((a,b) => {
-            if (a.name.toLowerCase() === b.name.toLowerCase()) {
-              return 0;
-            }
-            return (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1;
-          })
-        })
-    ).subscribe(val => {
-      this.apps = val;
-    });
-  }
+    ngOnInit() {
+        this.appsService.getAllApps().pipe(
+            map(apps => {
+                return apps
+                    .map(app => {
+                        // filter out deleted app versions
+                        app.appVersions = app.appVersions.filter(appVersion => appVersion.state !== ApplicationState.DELETED)
+                        return app
+                    })
+                    // filter out deleted apps or apps with no versions
+                    .filter(app => app.state !== ApplicationState.DELETED && app.appVersions.length >= 1)
+                    // sort by lowercase name
+                    .sort((a, b) => {
+                        if (a.name.toLowerCase() === b.name.toLowerCase()) {
+                            return 0;
+                        }
+                        return (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1;
+                    })
+            })
+        ).subscribe(val => {
+            this.apps = val;
+        });
+    }
 
-  public getStateAsString(state: any): string {
-    return typeof state === "string" && isNaN(Number(state.toString())) ? state: ApplicationState[state];
-  }
+    public getStateAsString(state: any): string {
+        return typeof state === "string" && isNaN(Number(state.toString())) ? state : ApplicationState[state];
+    }
 
-  public showModal(app:Application, appVersion: ApplicationVersion) : void {
-    this.selectedAppName = app.name;
-    this.selectedVersion = appVersion;
-    this.modal.show();
-  }
+    public showModal(app: Application, appVersion: ApplicationVersion): void {
+        this.selectedAppName = app.name;
+        this.selectedVersion = appVersion;
+        this.modal.show();
+    }
 
-  public clickTableRow(app: Application){
-    app.rowWithVersionVisible = !app.rowWithVersionVisible;
-  }
+    public clickTableRow(app: Application) {
+        app.rowWithVersionVisible = !app.rowWithVersionVisible;
+    }
 
-  public isAnySubtableVisible() : boolean {
-    return this.apps.filter(app => app.rowWithVersionVisible).length > 0;
-  }
+    public isAnySubtableVisible(): boolean {
+        return this.apps.filter(app => app.rowWithVersionVisible).length > 0;
+    }
 
 }
