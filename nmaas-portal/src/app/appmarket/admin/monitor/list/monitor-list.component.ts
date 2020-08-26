efcdceb5d9a39b2414d30cd7e4d265e4aeed1336
wtaisner@man.poznan.pl <Mzk5MTY0NTg1OTU0Ovu3YSgVZ+zprEjbjLbqnXY/ABt4>
2020-08-26 08:23:13 +0200

    NMAAS-878 assign actions to table rows and remove unnecessary action buttons

--- nmaas-portal/src/app/appmarket/admin/monitor/list/monitor-list.component.ts
+++ nmaas-portal/src/app/appmarket/admin/monitor/list/monitor-list.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {MonitorService} from "../../../../service/monitor.service";
-import {MonitorEntry, ServiceType, TimeFormat} from "../../../../model/monitorentry";
-import {TranslateService} from "@ngx-translate/core";
+import {MonitorService} from '../../../../service/monitor.service';
+import {MonitorEntry, ServiceType, TimeFormat} from '../../../../model/monitorentry';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-montiorlist',
@@ -12,7 +12,7 @@ export class MonitorListComponent implements OnInit {
 
   public monitorEntries: MonitorEntry[] = [];
 
-  private services: typeof ServiceType = ServiceType;
+  public services: typeof ServiceType = ServiceType;
 
   constructor(private monitorService: MonitorService, private translate: TranslateService) {
   }
@@ -21,36 +21,36 @@ export class MonitorListComponent implements OnInit {
     this.update();
   }
 
-  private update(){
+  private update() {
       this.monitorService.getAllMonitorEntries().subscribe(entries => this.monitorEntries = entries);
   }
 
-  public executeJob(serviceName: string){
+  public executeJob(serviceName: string) {
     this.monitorService.executeJob(serviceName).subscribe(val => this.update());
   }
 
-  public changeJobState(monitorEntry: MonitorEntry){
-    if(monitorEntry.active){
+  public changeJobState(monitorEntry: MonitorEntry) {
+    if (monitorEntry.active) {
       this.monitorService.pauseJob(monitorEntry.serviceName).subscribe(() => monitorEntry.active = false);
     } else {
       this.monitorService.resumeJob(monitorEntry.serviceName).subscribe(() => monitorEntry.active = true);
     }
   }
 
-  public getCorrectStateLabel(active: boolean) : string {
-    if(active){
+  public getCorrectStateLabel(active: boolean): string {
+    if (active) {
       return this.translate.instant('MONITOR.DEACTIVATE_BUTTON');
     }
     return this.translate.instant('MONITOR.ACTIVATE_BUTTON');
   }
 
-  public getIntervalCheck(checkInterval:number, timeFormat:TimeFormat):string{
-    if(checkInterval === 1 && timeFormat.toString() === TimeFormat[TimeFormat.MIN]){
-      return "1 minute";
-    } else if(checkInterval === 1 && timeFormat.toString() === TimeFormat[TimeFormat.H]){
-      return "1 hour";
+  public getIntervalCheck(checkInterval: number, timeFormat: TimeFormat): string {
+    if (checkInterval === 1 && timeFormat.toString() === TimeFormat[TimeFormat.MIN]) {
+      return '1 minute';
+    } else if (checkInterval === 1 && timeFormat.toString() === TimeFormat[TimeFormat.H]) {
+      return '1 hour';
     }
-    return timeFormat.toString() === TimeFormat[TimeFormat.MIN]? checkInterval + " minutes": checkInterval + " hours";
+    return timeFormat.toString() === TimeFormat[TimeFormat.MIN] ? checkInterval + ' minutes' : checkInterval + ' hours';
   }
 
 }
