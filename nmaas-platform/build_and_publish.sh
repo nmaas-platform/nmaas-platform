0e4ee258b8ee4f30ac72a55fd9d80821e93b1143
Lukasz Lopatowski <llopat@man.poznan.pl>
2020-09-28 07:51:24 +0200

    Updated core files for 1.3.7 development

--- nmaas-platform/build_and_publish.sh
+++ nmaas-platform/build_and_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=1.3.6
+TAG=1.3.7
 PACKAGE=nmaas-platform
 REPOSITORY=artifactory.geant.net/nmaas-docker-local
 sudo docker build --rm -t $REPOSITORY/$PACKAGE:$TAG -f ./Dockerfile ..
