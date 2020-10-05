b26ec51eb8d21c98d50c757be367bf58ff491670
wtaisner@man.poznan.pl <Mzk5MTY0NTg1OTU0Ovu3YSgVZ+zprEjbjLbqnXY/ABt4>
2020-10-05 13:29:32 +0200

    NMAAS-901 implement additional test bases

--- nmaas-portal/src/app/shared/common/domainfilter/domainfilter.component.spec.ts
+++ nmaas-portal/src/app/shared/common/domainfilter/domainfilter.component.spec.ts
@@ -1,25 +1,59 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DomainFilterComponent } from './domainfilter.component';
+import {DomainFilterComponent} from './domainfilter.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import createSpyObj = jasmine.createSpyObj;
+import {AppConfigService, DomainService} from '../../../service';
+import {AuthService} from '../../../auth/auth.service';
+import {UserDataService} from '../../../service/userdata.service';
+import {of} from 'rxjs';
 
 describe('DomainFilterComponent', () => {
-  let component: DomainFilterComponent;
-  let fixture: ComponentFixture<DomainFilterComponent>;
+    let component: DomainFilterComponent;
+    let fixture: ComponentFixture<DomainFilterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DomainFilterComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        const authServiceSpy = createSpyObj('AuthService', ['hasRole']);
+        authServiceSpy.hasRole.and.returnValue(true)
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DomainFilterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        const domainServiceSpy = createSpyObj('DomainService', ['getGlobalDomainId', 'getAll', 'getMyDomains'])
+        domainServiceSpy.getGlobalDomainId.and.returnValue(1)
+        domainServiceSpy.getAll.and.returnValue(of([]))
+        domainServiceSpy.getMyDomains.and.returnValue(of([]))
 
-  //it('should create', () => {
-  //  expect(component).toBeTruthy();
-  //});
+        TestBed.configureTestingModule({
+            declarations: [DomainFilterComponent],
+            imports: [
+                RouterTestingModule,
+                TranslateModule.forRoot({
+                    loader: {
+                        provide: TranslateLoader,
+                        useClass: TranslateFakeLoader
+                    }
+                }),
+            ],
+            providers: [
+                {provide: DomainService, useValue: domainServiceSpy},
+                {provide: AuthService, useValue: authServiceSpy},
+                {
+                    provide: UserDataService, useValue: {
+                        selectedDomainId: of(1)
+                    }
+                },
+                {provide: AppConfigService, useValue: {}},
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DomainFilterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- nmaas-portal/src/app/shared/users/list/userslist.component.spec.ts
+++ nmaas-portal/src/app/shared/users/list/userslist.component.spec.ts
@@ -1,55 +1,73 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UsersListComponent } from './userslist.component';
+import {UsersListComponent} from './userslist.component';
 import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {HttpClientModule} from '@angular/common/http';
-import {AppConfigService, DomainService, UserService} from '../../../service';
+import {DomainService, UserService} from '../../../service';
 import {UserDataService} from '../../../service/userdata.service';
 import {AuthService} from '../../../auth/auth.service';
 import {FormsModule} from '@angular/forms';
-import {JwtHelperService, JwtModule} from '@auth0/angular-jwt';
+import {JwtModule} from '@auth0/angular-jwt';
 import {NgxPaginationModule} from 'ngx-pagination';
+import {RouterTestingModule} from '@angular/router/testing';
+import {of} from 'rxjs';
+import createSpyObj = jasmine.createSpyObj;
 
 describe('UserslistComponent', () => {
-  let component: UsersListComponent;
-  let fixture: ComponentFixture<UsersListComponent>;
+    let component: UsersListComponent;
+    let fixture: ComponentFixture<UsersListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UsersListComponent ],
-      imports: [
-          TranslateModule.forRoot({
-              loader: {
-                  provide: TranslateLoader,
-                  useClass: TranslateFakeLoader
-              },
-          }),
-          JwtModule.forRoot({
-              config: {
-                  tokenGetter: () => {
-                      return '';
-                  }
-              }
-          }),
-          HttpClientModule,
-          FormsModule,
-          NgxPaginationModule
-      ],
-      providers: [ UserService, DomainService, UserDataService, AuthService, AppConfigService, JwtHelperService ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        const authServiceSpy = createSpyObj('AuthService', ['hasRole', 'hasDomainRole']);
+        authServiceSpy.hasRole.and.returnValue(true)
+        authServiceSpy.hasDomainRole.and.returnValue(true)
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UsersListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        const domainServiceSpy = createSpyObj('DomainService', ['getGlobalDomainId', 'getAll', 'getMyDomains'])
+        domainServiceSpy.getGlobalDomainId.and.returnValue(1)
+        domainServiceSpy.getAll.and.returnValue(of([]))
+        domainServiceSpy.getMyDomains.and.returnValue(of([]))
 
-  // TODO fix this badly mocked test
-  // it('should create', () => {
-  //     const app = fixture.debugElement.componentInstance;
-  //     expect(app).toBeTruthy();
-  // });
+        TestBed.configureTestingModule({
+            declarations: [UsersListComponent],
+            imports: [
+                TranslateModule.forRoot({
+                    loader: {
+                        provide: TranslateLoader,
+                        useClass: TranslateFakeLoader
+                    },
+                }),
+                JwtModule.forRoot({
+                    config: {
+                        tokenGetter: () => {
+                            return '';
+                        }
+                    }
+                }),
+                FormsModule,
+                NgxPaginationModule,
+                RouterTestingModule
+            ],
+            providers: [
+                {provide: DomainService, useValue: domainServiceSpy},
+                {provide: AuthService, useValue: authServiceSpy},
+                {
+                    provide: UserDataService, useValue: {
+                        selectedDomainId: of(1)
+                    }
+                },
+                {provide: UserService, useValue: {}},
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UsersListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
 });
