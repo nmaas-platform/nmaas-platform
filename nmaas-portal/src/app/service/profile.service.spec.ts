c0d0b0012cf18b175486042c967d64d02649fe93
wtaisner@man.poznan.pl <Mzk5MTY0NTg1OTU0Ovu3YSgVZ+zprEjbjLbqnXY/ABt4>
2020-10-07 07:57:58 +0200

    NMAAS-903 linting components

--- nmaas-portal/src/app/service/profile.service.spec.ts
+++ nmaas-portal/src/app/service/profile.service.spec.ts
@@ -1,13 +1,13 @@
 import {inject, TestBed} from '@angular/core/testing';
 
 import {ProfileService} from './profile.service';
-import {Observable, of} from "rxjs";
-import {Configuration} from "../model/configuration";
-import {HttpClient, HttpHandler} from "@angular/common/http";
-import {AppConfigService} from "./appconfig.service";
+import {Observable, of} from 'rxjs';
+import {Configuration} from '../model/configuration';
+import {AppConfigService} from './appconfig.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
-class MockConfigurationService{
-    protected uri:string;
+class MockConfigurationService {
+    protected uri: string;
 
     constructor() {
         this.uri = 'http://localhost/api';
@@ -17,23 +17,29 @@ class MockConfigurationService{
         return 'http://localhost/api';
     }
 
-    public getConfiguration():Observable<Configuration>{
+    public getConfiguration(): Observable<Configuration> {
         return of<Configuration>();
     }
 
-    public updateConfiguration(configuration:Configuration):Observable<any>{
+    public updateConfiguration(configuration: Configuration): Observable<any> {
         return of<Configuration>();
     }
 }
 
 describe('ProfileService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ProfileService, HttpHandler, HttpClient, {provide: AppConfigService, useClass: MockConfigurationService}]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            providers: [
+                ProfileService,
+                {provide: AppConfigService, useClass: MockConfigurationService}
+            ]
+        });
     });
-  });
 
-  it('should be created', inject([ProfileService], (service: ProfileService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([ProfileService], (service: ProfileService) => {
+        expect(service).toBeTruthy();
+    }));
 });
