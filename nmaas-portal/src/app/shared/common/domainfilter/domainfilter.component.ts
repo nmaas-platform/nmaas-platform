ed707257bf306c1540cf0865bdab1a4dc7592143
wtaisner@man.poznan.pl <Mzk5MTY0NTg1OTU0Ovu3YSgVZ+zprEjbjLbqnXY/ABt4>
2020-10-12 16:10:47 +0200

    NMAAS-910 add domain sorting functionality to domain filter component

--- nmaas-portal/src/app/shared/common/domainfilter/domainfilter.component.ts
+++ nmaas-portal/src/app/shared/common/domainfilter/domainfilter.component.ts
@@ -1,82 +1,119 @@
 import {AuthService} from '../../../auth/auth.service';
-import { Domain } from '../../../model/domain';
-import {DomainService, AppConfigService} from '../../../service';
+import {Domain} from '../../../model/domain';
+import {DomainService} from '../../../service';
 import {UserDataService} from '../../../service/userdata.service';
 import {Component, OnInit} from '@angular/core';
-import { Subscription ,  Observable } from 'rxjs';
+import {Subscription, Observable, of} from 'rxjs';
 
 import {map} from 'rxjs/operators';
 import {interval} from 'rxjs/internal/observable/interval';
 
 @Component({
-  selector: 'nmaas-domain-filter',
-  templateUrl: './domainfilter.component.html',
-  styleUrls: ['./domainfilter.component.css'],
+    selector: 'nmaas-domain-filter',
+    templateUrl: './domainfilter.component.html',
+    styleUrls: ['./domainfilter.component.css'],
 })
 export class DomainFilterComponent implements OnInit {
 
-  public domainId: number;
+    public domainId: number;
 
-  public domainName: string;
+    public domainName: string;
 
-  public domains: Observable<Domain[]>;
+    public domains: Observable<Domain[]>;
 
-  public refresh: Subscription;
+    public refresh: Subscription;
 
-  constructor(protected authService: AuthService,
-              protected domainService: DomainService,
-              protected userData: UserDataService,
-              protected appConfig: AppConfigService) {}
-
-  ngOnInit() {
-      if (this.authService.hasRole('ROLE_SYSTEM_ADMIN')) {
-        this.refresh = interval(10000).subscribe(next => {
-            if (this.domainService.shouldUpdate()) {
-                this.updateDomains();
-                this.domainService.setUpdateRequiredFlag(false);
-            }
-        });
-      }
-      this.updateDomains();
-      this.domains.subscribe(domain => {
-          this.domainName = domain[0].name;
-          this.userData.selectDomainId(domain[0].id)
-      });
-
-      this.userData.selectedDomainId.subscribe(id => this.domainId = id);
-  }
-
-  public updateDomains(): void {
-    if (this.authService.hasRole('ROLE_SYSTEM_ADMIN')) {
-      this.domains = this.domainService.getAll();
-    } else {
-      this.domains = this.domainService.getMyDomains();
-      const globalDomainId = this.appConfig.getNmaasGlobalDomainId();
-      if (this.domains !== undefined
-          && !this.authService.hasDomainRole(globalDomainId, 'ROLE_TOOL_MANAGER')
-          && !this.authService.hasDomainRole(globalDomainId, 'ROLE_OPERATOR')) {
-           this.domains = this.domains.pipe(
-               map(
-                   (domains) => domains.filter((domain) => domain.id !== globalDomainId && domain.active))
-           );
-      }
+    constructor(private authService: AuthService,
+                private domainService: DomainService,
+                private userData: UserDataService) {
     }
-  }
 
-  public onChange($event) {
-    console.log('onChange(', this.domainId, ')');
-    this.userData.selectDomainId(Number(this.domainId));
-  }
+    ngOnInit() {
+        if (this.authService.hasRole('ROLE_SYSTEM_ADMIN')) {
+            this.refresh = interval(10000).subscribe(next => {
+                if (this.domainService.shouldUpdate()) {
+                    this.updateDomains();
+                    this.domainService.setUpdateRequiredFlag(false);
+                }
+            });
+        }
+        this.updateDomains();
+        this.domains.subscribe(domain => {
+            this.domainName = domain[0].name;
+            this.userData.selectDomainId(domain[0].id)
+        });
 
-  public changeDomain(domain: number, dName: string) {
-    console.log('domainChange(', domain, ')');
-    this.domainId = domain;
-    this.domainName = dName;
-    this.userData.selectDomainId(Number(domain));
-  }
+        this.userData.selectedDomainId.subscribe(id => this.domainId = id);
+    }
 
-  public getCurrent() {
-    return this.domainName;
-  }
+    public updateDomains(): void {
+        if (this.authService.hasRole('ROLE_SYSTEM_ADMIN')) {
+            this.domains = this.domainService.getAll();
+        } else {
+            this.domains = this.domainService.getMyDomains();
+            const globalDomainId = this.domainService.getGlobalDomainId();
+            if (this.domains === undefined) {
+                this.domains = of([]);
+            }
+            if (!this.authService.hasDomainRole(globalDomainId, 'ROLE_TOOL_MANAGER')
+                && !this.authService.hasDomainRole(globalDomainId, 'ROLE_OPERATOR')) {
+                this.filterOutGlobalDomain();
+                this.filterOutNotActiveDomains();
+            }
+        }
+        this.sortDomains();
+    }
+
+    private filterOutNotActiveDomains(): void {
+        this.domains = this.domains.pipe(
+            map(
+                (domains) => domains.filter(domain => domain.active)
+            )
+        );
+    }
+
+    private filterOutGlobalDomain(): void {
+        const globalDomainId = this.domainService.getGlobalDomainId();
+        this.domains = this.domains.pipe(
+            map(
+                (domains) => domains.filter(domain => domain.id !== globalDomainId)
+            )
+        );
+    }
+
+    private sortDomains(): void {
+        const globalDomainId = this.domainService.getGlobalDomainId();
+        this.domains = this.domains.pipe(
+            map(
+                domains => {
+                    const global = domains.find(domain => domain.id === globalDomainId)
+                    domains = domains.filter(domain => domain.id !== globalDomainId)
+                    domains.sort((a: Domain, b: Domain): number => {
+                        return a.name.localeCompare(b.name)
+                    })
+                    if (global !== undefined) {
+                        domains.unshift(global)
+                    }
+                    return domains
+                }
+            )
+        )
+    }
+
+    public onChange($event) {
+        console.log('onChange(', this.domainId, ')');
+        this.userData.selectDomainId(Number(this.domainId));
+    }
+
+    public changeDomain(domain: number, dName: string) {
+        console.log('domainChange(', domain, ')');
+        this.domainId = domain;
+        this.domainName = dName;
+        this.userData.selectDomainId(Number(domain));
+    }
+
+    public getCurrent() {
+        return this.domainName;
+    }
 
 }
