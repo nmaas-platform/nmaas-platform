aafc0510c7c24406beab5e1c68cc02b387eff27b
Lukasz Lopatowski <llopat@man.poznan.pl>
2020-10-13 11:28:35 +0200

    Updated core files for version 1.4.0-SNAPSHOT

--- nmaas-portal/build_and_publish.sh
+++ nmaas-portal/build_and_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=1.3.7-SNAPSHOT
+TAG=1.4.0
 PACKAGE=nmaas-portal
 REPOSITORY=artifactory.geant.net/nmaas-docker-local
 sudo docker build --rm -t $REPOSITORY/$PACKAGE:$TAG -f ./Dockerfile ..
