412141b29534beb58f0aeba62064517569641ba7
wtaisner@man.poznan.pl <Mzk5MTY0NTg1OTU0Ovu3YSgVZ+zprEjbjLbqnXY/ABt4>
2020-11-10 10:48:33 +0100

    NMAAS-915 sort domains in admin domains list by name

--- nmaas-portal/src/app/appmarket/domains/list/domainslist.component.ts
+++ nmaas-portal/src/app/appmarket/domains/list/domainslist.component.ts
@@ -8,38 +8,55 @@ import {map} from 'rxjs/operators';
 import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-domains-list',
-  templateUrl: './domainslist.component.html',
-  styleUrls: ['./domainslist.component.css']
+    selector: 'app-domains-list',
+    templateUrl: './domainslist.component.html',
+    styleUrls: ['./domainslist.component.css']
 })
 export class DomainsListComponent implements OnInit {
 
-  public domains: Observable<Domain[]>;
+    public domains: Observable<Domain[]>;
 
-  constructor(protected domainService: DomainService, protected authService: AuthService, public translate: TranslateService) {
-  }
-
-  ngOnInit() {
-    this.update();
-  }
-
-  protected update(): void {
-    if (this.authService.hasRole(Role[Role.ROLE_SYSTEM_ADMIN]) || this.authService.hasRole(Role[Role.ROLE_OPERATOR])) {
-      this.domains = this.domainService.getAll().pipe(
-          map((domains) => domains.filter((domain) => domain.id !== this.domainService.getGlobalDomainId())));
-    } else {
-      this.domains = this.domainService.getMyDomains().pipe(
-          map((domains) => domains.filter((domain) => this.authService.hasDomainRole(domain.id, Role[Role.ROLE_DOMAIN_ADMIN]))));
+    constructor(protected domainService: DomainService, protected authService: AuthService, public translate: TranslateService) {
     }
-  }
 
-  public changeState(domain: Domain): void {
-    this.domainService.updateDomainState(domain).subscribe(() => this.update());
-    this.domainService.setUpdateRequiredFlag(true);
-  }
+    ngOnInit() {
+        this.update();
+    }
 
-  public getStateLabel(active: boolean) : string {
-    return active ? this.translate.instant("DOMAINS.DISABLE_BUTTON") : this.translate.instant("DOMAINS.ENABLE_BUTTON");
-  }
+    protected getDomainsObservable(): Observable<Domain[]> {
+        if (this.authService.hasRole(Role[Role.ROLE_SYSTEM_ADMIN]) || this.authService.hasRole(Role[Role.ROLE_OPERATOR])) {
+            return this.domainService.getAll().pipe(
+                map((domains) => domains.filter((domain) => domain.id !== this.domainService.getGlobalDomainId())));
+        } else {
+            return this.domainService.getMyDomains().pipe(
+                map((domains) => domains.filter((domain) => this.authService.hasDomainRole(domain.id, Role[Role.ROLE_DOMAIN_ADMIN]))));
+        }
+    }
+
+    protected update(): void {
+        this.domains = this.getDomainsObservable().pipe(
+            map((domains) => domains.sort(
+                (a, b) => {
+                    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                        return -1;
+                    }
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                        return 1;
+                    }
+                    return 0;
+                }
+                )
+            )
+        )
+    }
+
+    public changeState(domain: Domain): void {
+        this.domainService.updateDomainState(domain).subscribe(() => this.update());
+        this.domainService.setUpdateRequiredFlag(true);
+    }
+
+    public getStateLabel(active: boolean): string {
+        return active ? this.translate.instant('DOMAINS.DISABLE_BUTTON') : this.translate.instant('DOMAINS.ENABLE_BUTTON');
+    }
 
 }
