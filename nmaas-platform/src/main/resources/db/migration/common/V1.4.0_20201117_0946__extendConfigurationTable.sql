129533124bc118766263112e4cc06ee06476cdef
Lukasz Lopatowski <llopat@man.poznan.pl>
2020-11-17 21:22:07 +0100

    Fixed migration (data type)

--- nmaas-platform/src/main/resources/db/migration/common/V1.4.0_20201117_0946__extendConfigurationTable.sql
+++ nmaas-platform/src/main/resources/db/migration/common/V1.4.0_20201117_0946__extendConfigurationTable.sql
@@ -1,2 +1,2 @@
-alter table configuration add column app_instance_failure_emails varchar2(255) NOT NULL DEFAULT '';
+alter table configuration add column app_instance_failure_emails varchar(255) NOT NULL DEFAULT '';
 alter table configuration add column send_app_instance_failure_emails boolean NOT NULL DEFAULT FALSE;
