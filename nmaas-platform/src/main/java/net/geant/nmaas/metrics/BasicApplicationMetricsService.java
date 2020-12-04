92b096e7b67cbae789aef4e10f946a9da7ae5da3
Lukasz Lopatowski <llopat@man.poznan.pl>
2020-12-04 14:08:28 +0100

    Exposing additional application metrics

--- nmaas-platform/src/main/java/net/geant/nmaas/metrics/BasicApplicationMetricsService.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/metrics/BasicApplicationMetricsService.java
@@ -3,7 +3,7 @@ package net.geant.nmaas.metrics;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import net.geant.nmaas.portal.persistent.repositories.AppInstanceRepository;
-import net.geant.nmaas.portal.persistent.repositories.ApplicationRepository;
+import net.geant.nmaas.portal.persistent.repositories.ApplicationBaseRepository;
 import net.geant.nmaas.portal.persistent.repositories.ApplicationSubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,8 +14,11 @@ public class BasicApplicationMetricsService extends BaseMetricService {
     private static final String APPLICATION_COUNT_METRIC_NAME = "nmaas_applications_count";
     private static final String APPLICATION_COUNT_METRIC_DESCRIPTION = "Total NMaaS applications";
 
-    private static final String APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_NAME = "nmaas_application_instances_running_count";
-    private static final String APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_DESCRIPTION = "Total NMaaS running application instances";
+    private static final String APPLICATION_INSTANCE_RUNNING_ALL_COUNT_METRIC_NAME = "nmaas_application_instances_all_count";
+    private static final String APPLICATION_INSTANCE_RUNNING_ALL_COUNT_METRIC_DESCRIPTION = "Total NMaaS running application instances";
+
+    private static final String APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_NAME = "nmaas_application_instances_count";
+    private static final String APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_DESCRIPTION = "NMaaS running application instances";
 
     private static final String APPLICATION_SUBSCRIPTIONS_COUNT_METRIC_NAME = "nmaas_application_subscriptions_count";
     private static final String APPLICATION_SUBSCRIPTIONS_COUNT_METRIC_DESCRIPTION = "Total NMaaS application subscriptions";
@@ -23,7 +26,7 @@ public class BasicApplicationMetricsService extends BaseMetricService {
     private static final String BASE_UNIT_NUMBER = "number";
 
     @Autowired
-    private ApplicationRepository applicationRepository;
+    private ApplicationBaseRepository applicationBaseRepository;
 
     @Autowired
     private AppInstanceRepository appInstanceRepository;
@@ -33,14 +36,21 @@ public class BasicApplicationMetricsService extends BaseMetricService {
 
     @Override
     public void registerMetric(MeterRegistry registry) {
-        Gauge.builder(APPLICATION_COUNT_METRIC_NAME, applicationRepository, apps -> (double) apps.count())
+        Gauge.builder(APPLICATION_COUNT_METRIC_NAME, applicationBaseRepository, apps -> (double) apps.count())
                 .description(APPLICATION_COUNT_METRIC_DESCRIPTION)
                 .baseUnit(BASE_UNIT_NUMBER)
                 .register(registry);
-        Gauge.builder(APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_NAME, appInstanceRepository, instances -> (double) instances.countAllRunning())
-                .description(APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_DESCRIPTION)
+        Gauge.builder(APPLICATION_INSTANCE_RUNNING_ALL_COUNT_METRIC_NAME, appInstanceRepository, instances -> (double) instances.countAllRunning())
+                .description(APPLICATION_INSTANCE_RUNNING_ALL_COUNT_METRIC_DESCRIPTION)
                 .baseUnit(BASE_UNIT_NUMBER)
                 .register(registry);
+        for (String appName : applicationBaseRepository.findAllNames()) {
+            Gauge.builder(APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_NAME, appInstanceRepository, instances -> (double) instances.countRunningByName(appName))
+                    .description(APPLICATION_INSTANCE_RUNNING_COUNT_METRIC_DESCRIPTION)
+                    .baseUnit(BASE_UNIT_NUMBER)
+                    .tag("app", appName)
+                    .register(registry);
+        }
         Gauge.builder(APPLICATION_SUBSCRIPTIONS_COUNT_METRIC_NAME, applicationSubscriptionRepository, subscriptions -> (double) subscriptions.count())
                 .description(APPLICATION_SUBSCRIPTIONS_COUNT_METRIC_DESCRIPTION)
                 .baseUnit(BASE_UNIT_NUMBER)
--- nmaas-platform/src/main/java/net/geant/nmaas/portal/persistent/repositories/ApplicationBaseRepository.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/portal/persistent/repositories/ApplicationBaseRepository.java
@@ -1,12 +1,20 @@
 package net.geant.nmaas.portal.persistent.repositories;
 
+import java.util.List;
 import java.util.Optional;
 import net.geant.nmaas.portal.persistent.entity.ApplicationBase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationBaseRepository extends JpaRepository<ApplicationBase, Long> {
+
     boolean existsByName(String name);
+
     Optional<ApplicationBase> findByName(String name);
+
+    @Query("select distinct ab.name FROM ApplicationBase ab")
+    List<String> findAllNames();
+
 }
