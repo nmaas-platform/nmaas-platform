84489fffaa87ed9782f7ce77d9a9c841b955dc37
wtaisner@man.poznan.pl <wtaisner@man.poznan.pl>
2020-12-08 09:03:12 +0100

    NMAAS-935 fix one more sonar bug

--- nmaas-platform/src/main/java/net/geant/nmaas/portal/api/shell/observable/GenericShellSessionObservable.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/portal/api/shell/observable/GenericShellSessionObservable.java
@@ -2,9 +2,10 @@ package net.geant.nmaas.portal.api.shell.observable;
 
 import net.geant.nmaas.portal.api.shell.ShellCommandRequest;
 
+import java.io.Serializable;
 import java.util.Observable;
 
-public abstract class GenericShellSessionObservable extends Observable {
+public abstract class GenericShellSessionObservable extends Observable implements Serializable {
 
     public abstract void executeCommand(ShellCommandRequest commandRequest);
 
