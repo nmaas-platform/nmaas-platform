c670d33533057f87b89c5b075d23340ed7c64cc8
Lukasz Lopatowski <llopat@man.poznan.pl>
2021-01-11 23:45:33 +0100

    Added transaction to deal with user mapping errors

--- nmaas-platform/src/main/java/net/geant/nmaas/notifications/NotificationEventListener.java
+++ nmaas-platform/src/main/java/net/geant/nmaas/notifications/NotificationEventListener.java
@@ -3,6 +3,7 @@ package net.geant.nmaas.notifications;
 import lombok.AllArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -13,6 +14,7 @@ public class NotificationEventListener {
     private final NotificationManager notificationManager;
 
     @EventListener
+    @Transactional
     public void trigger(NotificationEvent event) {
         checkArgument(event.getMailAttributes() != null, "Mail attributes cannot be null");
         notificationManager.prepareAndSendMail(event.getMailAttributes());
