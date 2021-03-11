c832b842a9efcf07c25e2da147de0197809361c5
Lukasz Lopatowski <llopat@man.poznan.pl>
2021-03-11 11:57:52 +0100

    NMAAS-979: Added couple of tests

--- src/test/java/net/geant/nmaas/externalservices/inventory/kubernetes/KubernetesClusterIngressManagerTest.java
+++ src/test/java/net/geant/nmaas/externalservices/inventory/kubernetes/KubernetesClusterIngressManagerTest.java
@@ -2,19 +2,57 @@ package net.geant.nmaas.externalservices.inventory.kubernetes;
 
 import net.geant.nmaas.externalservices.inventory.kubernetes.model.IngressControllerConfigOption;
 import net.geant.nmaas.externalservices.inventory.kubernetes.model.IngressResourceConfigOption;
+import net.geant.nmaas.orchestration.entities.DomainTechDetails;
+import net.geant.nmaas.orchestration.repositories.DomainTechDetailsRepository;
+import net.geant.nmaas.portal.persistent.entity.Domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class KubernetesClusterIngressManagerTest {
+
+    private static final String DOMAIN = "testDomain";
+
+    private final DomainTechDetailsRepository domainTechDetailsRepository = mock(DomainTechDetailsRepository.class);
 
     private KubernetesClusterIngressManager manager;
 
     @BeforeEach
     public void setup() {
-        manager = new KubernetesClusterIngressManager();
+        manager = new KubernetesClusterIngressManager(domainTechDetailsRepository);
+    }
+
+    @Test
+    public void shouldReturnProperExternalServiceDomain() {
+        manager.setIngressPerDomain(false);
+        manager.setExternalServiceDomain("testClusterServiceDomain");
+        when(domainTechDetailsRepository.findByDomainCodename(DOMAIN)).thenReturn(Optional.empty());
+        assertThat(manager.getExternalServiceDomain(DOMAIN), is(manager.getKClusterIngressView().getExternalServiceDomain()));
+
+        DomainTechDetails domainTechDetails = new DomainTechDetails(1L, DOMAIN, "domainServiceDomain", "domainNamespace", "domainStorageClass", null);
+        Domain domain = new Domain("Domain Name", DOMAIN, false);
+        domain.setDomainTechDetails(domainTechDetails);
+        when(domainTechDetailsRepository.findByDomainCodename(DOMAIN)).thenReturn(Optional.of(domain.getDomainTechDetails()));
+        manager.setIngressPerDomain(true);
+        assertThat(manager.getExternalServiceDomain(DOMAIN), is(domain.getDomainTechDetails().getExternalServiceDomain()));
+    }
+
+    @Test
+    public void shouldThrowExceptionOnMissingDomain() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            manager.setIngressPerDomain(true);
+            manager.setExternalServiceDomain("testClusterServiceDomain");
+            when(domainTechDetailsRepository.findByDomainCodename(DOMAIN)).thenReturn(Optional.empty());
+            manager.getExternalServiceDomain(DOMAIN);
+        });
     }
 
     @Test
