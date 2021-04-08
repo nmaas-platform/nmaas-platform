0a65f5b33964d0c3503fa1aba0a9be1e7d6cba29
wtaisner@man.poznan.pl <wtaisner@man.poznan.pl>
2021-04-08 10:24:37 +0200

    NMAAS-967 add entity description

--- src/main/java/net/geant/nmaas/orchestration/entities/AppTermsAcceptance.java
+++ src/main/java/net/geant/nmaas/orchestration/entities/AppTermsAcceptance.java
@@ -8,6 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.OffsetDateTime;
 
+/**
+ * NMAAS-967 a record storing information about user terms acceptance
+ */
 @Entity
 @Getter
 @Setter
@@ -16,19 +19,40 @@ import java.time.OffsetDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AppTermsAcceptance {
 
+    /**
+     * artificial identifier
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Application name
+     */
     private String appName;
 
+    /**
+     * Application Instance ID (terms were accepted while deploying this instance)
+     */
     private Long appInstanceId;
 
+    /**
+     * Username of the user who accepted terms
+     */
     private String username;
 
+    /**
+     * Acceptance Statement - a word typed by user as acceptance
+     */
     private String termsAcceptanceStatement;
 
+    /**
+     * Content of terms to be accepted, e.g. license
+     */
     private String termsContent;
 
+    /**
+     * Timestamp of acceptance, generated at server side
+     */
     private OffsetDateTime acceptanceDate;
 }
