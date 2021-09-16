51b0ac18779efd47ce292ac5c86d52d595873377
Wojciech Taisner <wtaisner@man.poznan.pl>
2021-09-16 09:59:34 +0200

    NMAAS-1038 skip captcha check if secret not provided

--- src/main/java/net/geant/nmaas/utils/captcha/CaptchaValidator.java
+++ src/main/java/net/geant/nmaas/utils/captcha/CaptchaValidator.java
@@ -2,15 +2,18 @@ package net.geant.nmaas.utils.captcha;
 
 import java.util.Map;
 import java.util.Objects;
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Log4j2
 public class CaptchaValidator {
-
-    @Value("${captcha.secret}")
+    // set default value to `not_provided`
+    @Value("${captcha.secret:not_provided}")
     private String secret;
 
     private static final String GOOGLE_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
@@ -23,6 +26,10 @@ public class CaptchaValidator {
     }
 
     public boolean verifyToken(String token) {
+        if (secret.equalsIgnoreCase("not_provided")) {
+            log.info("Skipped captcha validation due to not provided token");
+            return true; // validate if secret is not provided
+        }
         final Map response = restTemplate.postForObject(prepareUrl(token), null, Map.class);
         return (boolean) Objects.requireNonNull(response).get("success");
     }
