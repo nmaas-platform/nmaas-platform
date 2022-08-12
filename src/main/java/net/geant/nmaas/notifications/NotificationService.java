74382239cd14676be7af2946e2eb266b34011347
Lukasz Lopatowski <llopat@man.poznan.pl>
2022-08-12 13:39:48 +0200

    NMAAS-1085: Added mail related timeout properties

--- src/main/java/net/geant/nmaas/notifications/NotificationService.java
+++ src/main/java/net/geant/nmaas/notifications/NotificationService.java
@@ -1,27 +1,36 @@
 package net.geant.nmaas.notifications;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
+@RequiredArgsConstructor
 class NotificationService {
 
     private final JavaMailSender mailSender;
 
-    @Autowired
-    NotificationService(JavaMailSender mailSender){
-        this.mailSender = mailSender;
+    void sendMail(String mail, String subject, String template, String fromAddress) {
+        executeSendMail(mail, subject, template, fromAddress);
     }
 
-    void sendMail(String mail, String subject, String template){
+    void sendMail(String mail, String subject, String template) {
+        executeSendMail(mail, subject, template, null);
+    }
+
+    private void executeSendMail(String mail, String subject, String template, String fromAddress) {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(mail);
             messageHelper.setSubject(subject);
             messageHelper.setText(template, true);
+            if (Objects.nonNull(fromAddress)) {
+                messageHelper.setFrom(fromAddress);
+            }
         };
         mailSender.send(messagePreparator);
     }
