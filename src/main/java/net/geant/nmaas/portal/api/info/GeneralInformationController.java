83329c4d2a1e6758236e50fa6513058c7f4ac3bb
Lukasz Lopatowski <llopat@man.poznan.pl>
2022-08-19 08:34:41 +0200

    NMAAS-1085: Removed commons.io dependency and refactored file handling in two places

--- src/main/java/net/geant/nmaas/portal/api/info/GeneralInformationController.java
+++ src/main/java/net/geant/nmaas/portal/api/info/GeneralInformationController.java
@@ -1,7 +1,6 @@
 package net.geant.nmaas.portal.api.info;
 
 import com.google.common.collect.ImmutableMap;
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.FileSystemResource;
@@ -10,9 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Map;
 
 @RestController
@@ -25,17 +26,21 @@ public class GeneralInformationController {
 
     @GetMapping(value = "/changelog", produces = "application/json")
     public FileSystemResource getChangelog() throws IOException {
-        File tempJsonFile = File.createTempFile("changelog",".json");
+        Path tempJsonFile = Files.createTempFile("changelog",".json");
         try(InputStream inputStream = changelogPath.getInputStream()) {
-            FileUtils.copyInputStreamToFile(inputStream, tempJsonFile);
+            Files.copy(inputStream, tempJsonFile, StandardCopyOption.REPLACE_EXISTING);
         }
         return new FileSystemResource(tempJsonFile);
     }
 
     @GetMapping(value = "/git")
     public Map<String, String> getGitInfo(@Value("${git.commit.time:none}") String buildTime, @Value("${git.commit.id.abbrev}") String commitName,
-                                          @Value("${git.build.version}") String buildVersion, @Value("${git.branch}") String branchName){
-        return ImmutableMap.of("buildTime",buildTime, "commitName",commitName, "buildVersion", buildVersion, "branchName", branchName);
+                                          @Value("${git.build.version}") String buildVersion, @Value("${git.branch}") String branchName) {
+        return ImmutableMap.of(
+                "buildTime", buildTime,
+                "commitName", commitName,
+                "buildVersion", buildVersion,
+                "branchName", branchName);
     }
 
 }
