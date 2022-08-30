f95fd5cdb329d994733884388cd02ced3c1a147e
Lukasz Lopatowski <llopat@man.poznan.pl>
2022-08-30 11:41:50 +0200

    Added userKey flag for WS scanning script

--- ws/run_ws.sh
+++ ws/run_ws.sh
@@ -4,4 +4,4 @@ echo "Downloading WhiteSource agent..."
 curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
 
 echo "Running WhiteSource scan..."
-java -jar wss-unified-agent.jar -apiKey ${API_KEY} -projectVersion ${PROJECT_VERSION} -projectToken ${PROJECT_TOKEN} -productVersion ${PRODUCT_VERSION} -productToken ${PRODUCT_TOKEN} -c ws.config -d ../
+java -jar wss-unified-agent.jar -userKey ${USER_KEY} -apiKey ${API_KEY} -projectVersion ${PROJECT_VERSION} -projectToken ${PROJECT_TOKEN} -productVersion ${PRODUCT_VERSION} -productToken ${PRODUCT_TOKEN} -c ws.config -d ../
