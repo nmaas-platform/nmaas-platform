d98e9d0f42a7a88b2679c311a324242527faf2a1
kbeyro <121854496+kbeyro@users.noreply.github.com>
2023-04-25 16:18:02 +0200

    add test for bulkDomainService

--- src/test/java/net/geant/nmaas/portal/service/impl/BulkDomainServiceImplTest.java
+++ src/test/java/net/geant/nmaas/portal/service/impl/BulkDomainServiceImplTest.java
@@ -64,6 +64,63 @@ public class BulkDomainServiceImplTest {
         assertEquals(responses.get(1).getCreated(), false);
         assertEquals(responses.get(1).getSuccessful(), true);
         assertEquals(responses.get(1).getType(), BulkType.USER);
+    }
 
+    @Test
+    void shouldHandleBulkCreationWhenDomainCreated() {
+        CsvDomain csvDomain = new CsvDomain("domain1", "user1", "user1@test.com", null, "group1");
+        List<CsvBean> input = List.of(new CsvDomain("domain1", "user1", "user1@test.com", null, "group1"));
+        Domain domain = new Domain(1L,"domain1", "domain1");
+        Domain global = new Domain(0L,"GLOBAL", "GLOBAL");
+        when(domainService.findDomain(anyString())).thenReturn(Optional.empty());
+        when(domainService.createDomain(any())).thenReturn(domain);
+        when(domainService.getGlobalDomain()).thenReturn(Optional.of(global));
+        DomainGroup domainGroup = new DomainGroup("group1", "group1");
+        when(domainGroupService.existDomainGroup("group1", "group1")).thenReturn(Boolean.TRUE);
+        when(userService.existsByUsername("user1")).thenReturn(Boolean.TRUE);
+        when(userService.existsByEmail("user1@test.com")).thenReturn(Boolean.FALSE);
+        User user = new User("user1", true);
+        user.setId(1L);
+        user.setEmail("user1@test.com");
+        when(userService.findByUsername("user1")).thenReturn(Optional.of(user));
+        when(userService.hasPrivilege(any(),any(),any())).thenReturn(true);
+        List<CsvProcessorResponse> responses = bulkDomainService.handleBulkCreation(input);
+        assertEquals(responses.size(), 2);
+        assertEquals(responses.get(0).getCreated(), true);
+        assertEquals(responses.get(0).getSuccessful(), true);
+        assertEquals(responses.get(0).getType(), BulkType.DOMAIN);
+        assertEquals(responses.get(1).getCreated(), false);
+        assertEquals(responses.get(1).getSuccessful(), true);
+        assertEquals(responses.get(1).getType(), BulkType.USER);
+    }
+
+    @Test
+    void shouldHandleBulkCreationWhenUserCreated() {
+        CsvDomain csvDomain = new CsvDomain("domain1", "user1", "user1@test.com", null, "group1");
+        List<CsvBean> input = List.of(new CsvDomain("domain1", "user1", "user1@test.com", null, "group1"));
+        Domain domain = new Domain(1L,"domain1", "domain1");
+        Domain global = new Domain(0L,"GLOBAL", "GLOBAL");
+        when(domainService.findDomain(anyString())).thenReturn(Optional.empty());
+        when(domainService.createDomain(any())).thenReturn(domain);
+        when(domainService.getGlobalDomain()).thenReturn(Optional.of(global));
+        DomainGroup domainGroup = new DomainGroup("group1", "group1");
+        when(domainGroupService.existDomainGroup("group1", "group1")).thenReturn(Boolean.TRUE);
+        when(userService.existsByUsername("user1")).thenReturn(false);
+        when(userService.existsByEmail("user1@test.com")).thenReturn(Boolean.FALSE);
+        User user = new User("user1", true);
+        user.setId(10L);
+        user.setEmail("user1@test.com");
+        when(userService.registerBulk(any(), any(), any())).thenReturn(user);
+        List<CsvProcessorResponse> responses = bulkDomainService.handleBulkCreation(input);
+        assertEquals(responses.size(), 2);
+        assertEquals(responses.get(0).getCreated(), true);
+        assertEquals(responses.get(0).getSuccessful(), true);
+        assertEquals(responses.get(0).getType(), BulkType.DOMAIN);
+        assertEquals(responses.get(1).getCreated(), true);
+        assertEquals(responses.get(1).getSuccessful(), true);
+        assertEquals(responses.get(1).getType(), BulkType.USER);
+        assertEquals(responses.get(1).getDetails().get("userName"), "user1");
+        assertEquals(responses.get(1).getDetails().get("userId"), "10");
+        assertEquals(responses.get(1).getDetails().get("email"), "user1@test.com");
     }
 }
