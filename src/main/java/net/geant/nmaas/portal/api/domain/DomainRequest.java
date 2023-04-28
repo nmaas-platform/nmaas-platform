427698b8d543ba8c63384c987f18a21d0c3c106b
kbeyro <121854496+kbeyro@users.noreply.github.com>
2023-04-28 16:13:02 +0200

    Add tech and DCN details in bulk

--- src/main/java/net/geant/nmaas/portal/api/domain/DomainRequest.java
+++ src/main/java/net/geant/nmaas/portal/api/domain/DomainRequest.java
@@ -1,6 +1,7 @@
 package net.geant.nmaas.portal.api.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class DomainRequest {
 
 	@NotNull
