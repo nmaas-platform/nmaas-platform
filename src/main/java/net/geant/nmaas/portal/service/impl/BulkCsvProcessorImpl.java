dc06b9af452edd4319db50f2e5b7f630e97a01bb
kbeyro <121854496+kbeyro@users.noreply.github.com>
2023-08-17 13:08:38 +0200

    Add support for handling header errors

--- src/main/java/net/geant/nmaas/portal/service/impl/BulkCsvProcessorImpl.java
+++ src/main/java/net/geant/nmaas/portal/service/impl/BulkCsvProcessorImpl.java
@@ -19,7 +19,6 @@ import java.io.OutputStream;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,7 +38,7 @@ public class BulkCsvProcessorImpl implements BulkCsvProcessor {
     @Override
     public List<CsvDomain> processDomainSpecs(MultipartFile file) throws IOException {
         List<CsvDomain> result = process(file, CsvDomain.class).stream().map(d -> (CsvDomain) d).collect(Collectors.toList());
-        if(result.isEmpty()) {
+        if (result.isEmpty()) {
             throw new ProcessingException("CSV file contains 0 entries");
         }
         return result;
@@ -48,7 +47,7 @@ public class BulkCsvProcessorImpl implements BulkCsvProcessor {
     @Override
     public List<CsvApplication> processApplicationSpecs(MultipartFile file) throws IOException {
         List<CsvApplication> result = process(file, CsvApplication.class).stream().map(d -> (CsvApplication) d).collect(Collectors.toList());
-        if(result.isEmpty()) {
+        if (result.isEmpty()) {
             throw new ProcessingException("CSV file contains 0 entries");
         }
         return result;
@@ -56,7 +55,8 @@ public class BulkCsvProcessorImpl implements BulkCsvProcessor {
 
     /**
      * Read CSV file and map it to given class type
-     * @param file an MultipartFile CSV from controller
+     *
+     * @param file       an MultipartFile CSV from controller
      * @param outputType an CSVClass created for reader of CSV file (used to map fields)
      * @throws IOException thrown when provided file is invalid
      */
@@ -95,16 +95,17 @@ public class BulkCsvProcessorImpl implements BulkCsvProcessor {
         return TYPE_CSV.equals(file.getContentType());
     }
 
-    public interface CsvBean {}
+    public interface CsvBean {
+    }
 
     private static void validateHeaderColumns(String[] headers) {
         log.debug("List = {}", List.of(headers));
 
-        if(headers.length == 0) {
+        if (headers.length == 0) {
             throw new ProcessingException("CSV headers are empty");
         }
 
-        if(Arrays.asList(headers).containsAll(requiredDomainHeader) || Arrays.asList(headers).containsAll(requireApplicationHeader)) {
+        if (Arrays.asList(headers).containsAll(requiredDomainHeader) || Arrays.asList(headers).containsAll(requireApplicationHeader)) {
             log.debug("Nagłówki są poprawne");
         } else {
             throw new ProcessingException("CSV headers missing required column");
