519c1537008a2c592b26f9d9bb3f68c5fdb40dda
kbeyro <121854496+kbeyro@users.noreply.github.com>
2023-08-17 13:08:07 +0200

    Add support for handling header errors

--- src/main/java/net/geant/nmaas/portal/service/impl/BulkCsvProcessorImpl.java
+++ src/main/java/net/geant/nmaas/portal/service/impl/BulkCsvProcessorImpl.java
@@ -1,11 +1,13 @@
 package net.geant.nmaas.portal.service.impl;
 
+import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.geant.nmaas.portal.api.bulk.CsvApplication;
 import net.geant.nmaas.portal.api.bulk.CsvDomain;
+import net.geant.nmaas.portal.api.exception.ProcessingException;
 import net.geant.nmaas.portal.service.BulkCsvProcessor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +19,8 @@ import java.io.OutputStream;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,14 +31,27 @@ public class BulkCsvProcessorImpl implements BulkCsvProcessor {
 
     public static final String TYPE_CSV = "text/csv";
 
+    private static final List<String> requiredDomainHeader = List.of("domain", "username", "email");
+
+    private static final List<String> requireApplicationHeader = List.of("domain", "instance", "version");
+
+
     @Override
     public List<CsvDomain> processDomainSpecs(MultipartFile file) throws IOException {
-        return process(file, CsvDomain.class).stream().map(d -> (CsvDomain) d).collect(Collectors.toList());
+        List<CsvDomain> result = process(file, CsvDomain.class).stream().map(d -> (CsvDomain) d).collect(Collectors.toList());
+        if(result.isEmpty()) {
+            throw new ProcessingException("CSV file contains 0 entries");
+        }
+        return result;
     }
 
     @Override
     public List<CsvApplication> processApplicationSpecs(MultipartFile file) throws IOException {
-        return process(file, CsvApplication.class).stream().map(d -> (CsvApplication) d).collect(Collectors.toList());
+        List<CsvApplication> result = process(file, CsvApplication.class).stream().map(d -> (CsvApplication) d).collect(Collectors.toList());
+        if(result.isEmpty()) {
+            throw new ProcessingException("CSV file contains 0 entries");
+        }
+        return result;
     }
 
     /**
@@ -59,6 +76,12 @@ public class BulkCsvProcessorImpl implements BulkCsvProcessor {
     }
 
     private static List<CsvBean> beanBuilderExample(Path path, Class clazz) throws Exception {
+
+        try (Reader reader = Files.newBufferedReader(path)) {
+            CSVReader csvReader = new CSVReader(reader);
+            validateHeaderColumns(csvReader.readNext());
+        }
+
         try (Reader reader = Files.newBufferedReader(path)) {
             CsvToBean cb = new CsvToBeanBuilder<CsvBean>(reader)
                     .withType(clazz)
@@ -73,5 +96,19 @@ public class BulkCsvProcessorImpl implements BulkCsvProcessor {
     }
 
     public interface CsvBean {}
+
+    private static void validateHeaderColumns(String[] headers) {
+        log.debug("List = {}", List.of(headers));
+
+        if(headers.length == 0) {
+            throw new ProcessingException("CSV headers are empty");
+        }
+
+        if(Arrays.asList(headers).containsAll(requiredDomainHeader) || Arrays.asList(headers).containsAll(requireApplicationHeader)) {
+            log.debug("Nagłówki są poprawne");
+        } else {
+            throw new ProcessingException("CSV headers missing required column");
+        }
+    }
 
 }
