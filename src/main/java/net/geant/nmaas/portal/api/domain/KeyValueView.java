6eb31e9fd5a0337b02ecabd20935eefddb0ed1b3
Łukasz Łopatowski <lukasz.lopatowski@gmail.com>
2024-03-07 13:49:14 +0100

    Removed not used custom annotation related flag. Updated grpc dependencies.

--- src/main/java/net/geant/nmaas/portal/api/domain/KeyValueView.java
+++ src/main/java/net/geant/nmaas/portal/api/domain/KeyValueView.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 
@@ -13,6 +14,7 @@ import javax.persistence.Column;
 @Setter
 @Getter
 @Builder
+@ToString
 public class KeyValueView {
 
     @Column(name = "key_string")
