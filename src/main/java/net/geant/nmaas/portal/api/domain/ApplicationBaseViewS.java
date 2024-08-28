f8f88bdf9be9076384f398826f565f63ae307ae6
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-08-28 12:47:43 +0200

    Add model AppBaseViewS

---
+++ src/main/java/net/geant/nmaas/portal/api/domain/ApplicationBaseViewS.java
@@ -0,0 +1,29 @@
+package net.geant.nmaas.portal.api.domain;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class ApplicationBaseViewS {
+
+    protected Long id;
+    @NotEmpty
+    protected String name;
+
+    @Builder.Default
+    protected List<AppDescriptionView> descriptions = new ArrayList<>();
+    @Builder.Default
+    protected Set<TagView> tags = new HashSet<>();
+
+    protected AppRateView rate;
+}
