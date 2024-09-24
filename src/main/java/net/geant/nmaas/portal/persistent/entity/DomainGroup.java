a285b92ed238f534c4215c01a9e2efff389250ef
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-09-24 12:20:15 +0200

    delete appbase from appstateperdomain

--- src/main/java/net/geant/nmaas/portal/persistent/entity/DomainGroup.java
+++ src/main/java/net/geant/nmaas/portal/persistent/entity/DomainGroup.java
@@ -23,6 +23,7 @@ import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Entity
