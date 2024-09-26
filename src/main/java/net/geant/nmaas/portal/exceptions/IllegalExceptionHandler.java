3cfc22afa3915af95f494e01ff2323de83c64ef0
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-09-26 13:14:08 +0200

    update handler

--- src/main/java/net/geant/nmaas/portal/exceptions/IllegalExceptionHandler.java
+++ src/main/java/net/geant/nmaas/portal/exceptions/IllegalExceptionHandler.java
@@ -1,20 +1,31 @@
 package net.geant.nmaas.portal.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.pool.TypePool;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class IllegalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> handle(IllegalArgumentException ex) {
-        throw new ObjectAlreadyExistsException(ex.getMessage());
+        log.error("Test throw ? ");
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<?> handle(IllegalStateException ex) {
-        throw new ObjectAlreadyExistsException(ex.getMessage());
+        log.error("Test throw 2 ? ");
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handle(RuntimeException ex) {
+        log.error("Test throw 3 ? ");
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_ACCEPTABLE);
     }
 }
