85f990af5940b682f9b7e136ab0dc812337ce260
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-10-01 11:28:34 +0200

    delete import

--- src/main/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/HelmRepoUpdateListener.java
+++ src/main/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/HelmRepoUpdateListener.java
@@ -3,7 +3,6 @@ package net.geant.nmaas.nmservice.deployment.containerorchestrators.kubernetes;
 import lombok.RequiredArgsConstructor;
 import net.geant.nmaas.nmservice.deployment.containerorchestrators.kubernetes.components.helm.HelmKServiceManager;
 import net.geant.nmaas.portal.events.ApplicationActivatedEvent;
-import net.geant.nmaas.portal.events.ApplicationListUpdatedEvent;
 import net.geant.nmaas.utils.logging.LogLevel;
 import net.geant.nmaas.utils.logging.Loggable;
 import org.springframework.context.ApplicationEvent;
