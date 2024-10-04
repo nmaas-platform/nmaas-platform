84898d40720b66774f9112e2bca6a95a72f3f300
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-10-04 16:14:05 +0200

    fix missing commit

--- src/main/java/net/geant/nmaas/portal/service/impl/BulkApplicationServiceImpl.java
+++ src/main/java/net/geant/nmaas/portal/service/impl/BulkApplicationServiceImpl.java
@@ -57,6 +57,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
