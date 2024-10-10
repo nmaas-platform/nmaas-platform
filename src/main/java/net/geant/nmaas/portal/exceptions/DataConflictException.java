1d98751c1d9b1f3c69c88787d6ca97ac7111b8cf
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-10-10 13:00:56 +0200

    Add exception for contact types

--- src/main/java/net/geant/nmaas/portal/exceptions/DataConflictException.java
+++ src/main/java/net/geant/nmaas/portal/exceptions/DataConflictException.java
@@ -5,16 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DataConflictException extends RuntimeException {
 
-    private String message;
-    public DataConflictException(String message)
-    {
+    public DataConflictException(String message) {
         super(message);
-        log.error("test ?????");
-        this.message = message;
-    }
-
-    public String getMsg() {
-        return message;
     }
 
 }
