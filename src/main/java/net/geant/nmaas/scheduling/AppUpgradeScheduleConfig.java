99594902aa95a15bccd4776f3303a54a9bae06dd
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-10-30 14:29:23 +0100

    Add job config

--- src/main/java/net/geant/nmaas/scheduling/AppUpgradeScheduleConfig.java
+++ src/main/java/net/geant/nmaas/scheduling/AppUpgradeScheduleConfig.java
@@ -2,6 +2,8 @@ package net.geant.nmaas.scheduling;
 
 import com.google.common.base.Strings;
 import lombok.extern.log4j.Log4j2;
+import net.geant.nmaas.nmservice.deployment.bulks.BulkDeploymentJob;
+import net.geant.nmaas.nmservice.deployment.bulks.BulkDeploymentJobService;
 import net.geant.nmaas.orchestration.AppUpgradeSummaryJob;
 import net.geant.nmaas.orchestration.AppUpgradeTriggerJob;
 import org.springframework.beans.factory.InitializingBean;
@@ -17,6 +19,8 @@ public class AppUpgradeScheduleConfig {
 
     private static final String APP_UPGRADE_JOB_NAME = "AppUpgradeJob";
     private static final String APP_UPGRADE_SUMMARY_JOB_NAME = "AppUpgradeSummaryJob";
+    private static final String BULK_DEPLOYMENT_JOB = "BulkDeploymentJob";
+
 
     @Bean
     public InitializingBean insertDefaultAppUpgradeJob() {
@@ -29,6 +33,9 @@ public class AppUpgradeScheduleConfig {
             private AppUpgradeSummaryJob appUpgradeSummaryJob;
 
             @Autowired
+            private BulkDeploymentJob bulkDeploymentJob;
+
+            @Autowired
             private ScheduleManager scheduleManager;
 
             @Value("${nmaas.service.upgrade.cron}")
@@ -36,6 +43,9 @@ public class AppUpgradeScheduleConfig {
 
             @Value("${nmaas.service.upgrade-summary.cron}")
             String appUpgradeSummaryCron;
+
+            @Value("${nmaas.service.bulk-deployment.cron}")
+            String bulkDeploymentCron;
 
             @Override
             @Transactional
@@ -51,6 +61,12 @@ public class AppUpgradeScheduleConfig {
                     log.warn("Won't send out email notifications about automatic upgrades in given period");
                 } else {
                     this.scheduleManager.createJob(appUpgradeSummaryJob, APP_UPGRADE_SUMMARY_JOB_NAME, appUpgradeSummaryCron);
+                }
+                if (Strings.isNullOrEmpty(bulkDeploymentCron)) {
+                    log.warn("Bulk deployment cron expression not provided");
+                } else {
+                    log.warn("Created bulk deplyoment cron ");
+                    this.scheduleManager.createJob(bulkDeploymentJob, BULK_DEPLOYMENT_JOB, bulkDeploymentCron);
                 }
             }
         };
