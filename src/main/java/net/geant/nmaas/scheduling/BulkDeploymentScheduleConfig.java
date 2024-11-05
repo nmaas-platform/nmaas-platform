c08f4b5bf2545961e66120fc50298a2dbc3e94d0
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-11-05 14:49:58 +0100

    update scheduler

--- src/main/java/net/geant/nmaas/scheduling/BulkDeploymentScheduleConfig.java
+++ src/main/java/net/geant/nmaas/scheduling/BulkDeploymentScheduleConfig.java
@@ -3,6 +3,7 @@ package net.geant.nmaas.scheduling;
 import com.google.common.base.Strings;
 import lombok.extern.log4j.Log4j2;
 import net.geant.nmaas.nmservice.deployment.bulks.BulkDeploymentJob;
+import net.geant.nmaas.portal.service.ConfigurationManager;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,13 +33,20 @@ public class BulkDeploymentScheduleConfig {
             @Autowired
             private ScheduleManager scheduleManager;
 
+            @Autowired
+            private ConfigurationManager configurationManager;
+
             @Value("${nmaas.service.bulk-deployment.cron}")
             String bulkDeploymentCron;
 
             @Override
             @Transactional
             public void afterPropertiesSet() {
-                if (Strings.isNullOrEmpty(bulkDeploymentCron)) {
+                if(!Strings.isNullOrEmpty(configurationManager.getConfiguration().getBulkDeploymentJobCron())) {
+                    log.debug("Configuration is loaded, taking bulk cron setting from there");
+                    this.scheduleManager.createJob(bulkDeploymentJob, BULK_DEPLOYMENT_JOB, configurationManager.getConfiguration().getBulkDeploymentJobCron());
+                }
+                else if (Strings.isNullOrEmpty(bulkDeploymentCron)) {
                     log.warn("Bulk deployment cron expression not provided");
                 } else {
                     this.scheduleManager.createJob(bulkDeploymentJob, BULK_DEPLOYMENT_JOB, bulkDeploymentCron);
