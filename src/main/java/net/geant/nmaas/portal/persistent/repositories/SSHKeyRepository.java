a1ffb8f8ef58b5897830f85df4061c2c51fdff3a
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-01-10 14:27:00 +0100

    block adding same shhkey for different users

--- src/main/java/net/geant/nmaas/portal/persistent/repositories/SSHKeyRepository.java
+++ src/main/java/net/geant/nmaas/portal/persistent/repositories/SSHKeyRepository.java
@@ -3,6 +3,8 @@ package net.geant.nmaas.portal.persistent.repositories;
 import net.geant.nmaas.portal.persistent.entity.SSHKeyEntity;
 import net.geant.nmaas.portal.persistent.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface SSHKeyRepository extends JpaRepository<SSHKeyEntity, Long> {
 
     List<SSHKeyEntity> findAllByOwner(User owner);
     boolean existsByOwnerAndName(User owner, String name);
+
+//    @Query("Select k from SSHKeyEntity k where k.keyValue =: keyValue")
+    boolean existsByKeyValue(@Param("keyValue") String keyValue);
 }
