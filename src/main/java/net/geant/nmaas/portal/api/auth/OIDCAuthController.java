91ee9c87b6637994cef345a82b9722bc11509404
pkazimierowski <pkazimierowski@man.poznan.pl>
2025-01-15 11:18:07 +0100

    added new oidc endpoint

--- src/main/java/net/geant/nmaas/portal/api/auth/OIDCAuthController.java
+++ src/main/java/net/geant/nmaas/portal/api/auth/OIDCAuthController.java
@@ -1,12 +1,12 @@
 package net.geant.nmaas.portal.api.auth;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.geant.nmaas.portal.api.exception.MissingElementException;
 import net.geant.nmaas.portal.api.exception.SignupException;
 import net.geant.nmaas.portal.api.security.JWTTokenService;
 import net.geant.nmaas.portal.exceptions.ObjectAlreadyExistsException;
-import net.geant.nmaas.portal.persistent.entity.Domain;
 import net.geant.nmaas.portal.persistent.entity.User;
 import net.geant.nmaas.portal.service.DomainService;
 import net.geant.nmaas.portal.service.UserService;
@@ -14,11 +14,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.List;
+import java.io.IOException;
 
 
 @RestController
@@ -40,11 +41,20 @@ public class OIDCAuthController {
     @GetMapping("/oidc/success")
     public RedirectView oidcLoginSuccess(@AuthenticationPrincipal OidcUser oidcUserser) {
 
-        User user = userService.existsByUsername("oidc_" + oidcUserser.getAttribute("preferred_username")) ?
-                userService.findByUsername("oidc_" + oidcUserser.getAttribute("preferred_username")).orElseThrow()
+        User user = userService
+                .existsByUsername("oidc_"
+                        + oidcUserser.getAttribute("preferred_username"))
+                ? userService
+                .findByUsername("oidc_"
+                        + oidcUserser.getAttribute("preferred_username"))
+                .orElseThrow()
                 : registerNewUser(oidcUserser);
 
-        String redirectUrl = portalAddress +  "/login-success?token=" + jwtTokenService.getToken(user) + "&refresh_token=" + jwtTokenService.getRefreshToken(user);
+        String redirectUrl = portalAddress
+                + "/login-success?token="
+                + jwtTokenService.getToken(user)
+                + "&refresh_token="
+                + jwtTokenService.getRefreshToken(user);
         return new RedirectView(redirectUrl);
 
     }
@@ -52,7 +62,10 @@ public class OIDCAuthController {
     private User registerNewUser(OidcUser oidcUserser) {
 
         try {
-            return userService.register(oidcUserser, domains.getGlobalDomain().orElseThrow(MissingElementException::new));
+            return userService
+                    .register(oidcUserser,
+                            domains.getGlobalDomain().orElseThrow(MissingElementException::new)
+                    );
         } catch (ObjectAlreadyExistsException e) {
             throw new SignupException("User already exists");
         } catch (MissingElementException e) {
@@ -60,4 +73,13 @@ public class OIDCAuthController {
         }
     }
 
+    @GetMapping("/api/oauth2/authorization/{registrationId}")
+    public void redirectToAuthorization(
+            @PathVariable String registrationId,
+            HttpServletResponse response) throws IOException {
+        String redirectUri = "/oauth2/authorization/" + registrationId;
+        response.sendRedirect(redirectUri);
+    }
 }
+
+
