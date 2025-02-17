3f7a8bf9e33badcbeaa9dc0d49f4f41a84b116a3
Lukasz Lopatowski <llopat@man.poznan.pl>
2025-02-17 14:16:52 +0100

    Minor fix in migration

--- src/main/resources/db/migration/common/V1.7.0_20250217_1200__ChangeMonitorServiceNameType.sql
+++ src/main/resources/db/migration/common/V1.7.0_20250217_1200__ChangeMonitorServiceNameType.sql
@@ -1,2 +1,2 @@
-ALTER TABLE  monitor_entry ALTER COLUMN service_name TYPE varchar(255) not null;
+ALTER TABLE monitor_entry ALTER COLUMN service_name TYPE varchar(255) not null;
 ALTER TABLE monitor_entry ALTER COLUMN status TYPE varchar(255) not null;
