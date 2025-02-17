4b21f4b15053e614c2c8a7f8c0698f57ab5f3a83
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-02-17 12:46:04 +0100

    change enum to string in database

--- src/main/resources/db/migration/common/V1.7.0_20250217_1200__ChangeMonitorServiceNameType.sql
+++ src/main/resources/db/migration/common/V1.7.0_20250217_1200__ChangeMonitorServiceNameType.sql
@@ -1,2 +1,5 @@
 ALTER TABLE  monitor_entry DROP COLUMN service_name;
 ALTER TABLE monitor_entry ADD COLUMN service_name varchar(255) not null;
+
+ALTER TABLE  monitor_entry DROP COLUMN status;
+ALTER TABLE monitor_entry ADD COLUMN status varchar(255) not null;
