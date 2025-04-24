f280f5df92e93bb6a74b55d608674785e42b7c2a
Lukasz Lopatowski <llopat@man.poznan.pl>
2025-04-24 16:49:01 +0200

    Added validation of command execution

--- src/main/java/net/geant/nmaas/utils/bash/DefaultCommandExecutor.java
+++ src/main/java/net/geant/nmaas/utils/bash/DefaultCommandExecutor.java
@@ -1,10 +1,12 @@
 package net.geant.nmaas.utils.bash;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Predicate;
 
 @Slf4j
 @Component
@@ -12,22 +14,33 @@ public class DefaultCommandExecutor implements CommandExecutor {
 
     @Override
     public void execute(Command command) throws CommandExecutionException {
+        executeInternal(command);
+    }
+
+    @Override
+    public String executeWithOutput(Command command) throws CommandExecutionException {
+        return executeInternal(command);
+    }
+
+    private String executeInternal(Command command) throws CommandExecutionException {
         try {
-            new ProcessBuilder(command.asString().split(" ")).start();
+            log.info("Executing: {}", command);
+            Process process = new ProcessBuilder(command.asString().split(" ")).start();
+            final String errorOutput = new String(process.getErrorStream().readAllBytes(), StandardCharsets.UTF_8);
+            if (Strings.isNotEmpty(errorOutput)) {
+                throw new CommandExecutionException("Error received during command execution (details: " + errorOutput + ")");
+            }
+            final String output = new String(process.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            validateOutput(output, command.isOutputCorrect());
+            return output;
         } catch (IOException e) {
             throw new CommandExecutionException(e.getMessage());
         }
     }
 
-    @Override
-    public String executeWithOutput(Command command) throws CommandExecutionException {
-        try {
-            Process process = new ProcessBuilder(command.asString().split(" ")).start();
-            final String output = new String(process.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
-            log.debug("Output: {}", output);
-            return output;
-        } catch (IOException e) {
-            throw new CommandExecutionException(e.getMessage());
+    private static void validateOutput(String output, Predicate<String> isOutputCorrect) {
+        if (isOutputCorrect.negate().test(output)) {
+            throw new CommandExecutionException("Identified problem with command execution based on output (details: " + output + ")");
         }
     }
 
--- src/test/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/components/helm/HelmCommandExecutorTest.java
+++ src/test/java/net/geant/nmaas/nmservice/deployment/containerorchestrators/kubernetes/components/helm/HelmCommandExecutorTest.java
@@ -9,61 +9,61 @@ public class HelmCommandExecutorTest {
 
     private static final String EXAMPLE_HELM_STATUS_COMMAND_OUTPUT =
             "LAST DEPLOYED: Wed Dec  6 13:48:59 2017\n" +
-            "NAMESPACE: default\n" +
-            "STATUS: DEPLOYED\n" +
-            "" +
-            "RESOURCES:" +
-            "==> v1/PersistentVolumeClaim" +
-            "NAME                                  STATUS  VOLUME                                    CAPACITY  ACCESSMODES  STORAGECLASS         AGE" +
-            "c21584cd-666c-42de-9df7-d72b7bae5aae  Bound   pvc-d4eb67d2-da83-11e7-bec9-5254002cd33f  1Gi       RWO          managed-nfs-storage  1m" +
-            "" +
-            "==> v1/Service" +
-            "NAME                                                 CLUSTER-IP    EXTERNAL-IP  PORT(S)  AGE" +
-            "c21584cd-666c-42de-9df7-d72b7bae5aae-nmaas-oxidized  10.13.82.246  <none>       80/TCP   1m" +
-            "" +
-            "==> v1beta1/Deployment" +
-            "NAME                                                 DESIRED  CURRENT  UP-TO-DATE  AVAILABLE  AGE" +
-            "c21584cd-666c-42de-9df7-d72b7bae5aae-nmaas-oxidized  1        1        1           1          1m" +
-            "";
+                    "NAMESPACE: default\n" +
+                    "STATUS: DEPLOYED\n" +
+                    "" +
+                    "RESOURCES:" +
+                    "==> v1/PersistentVolumeClaim" +
+                    "NAME                                  STATUS  VOLUME                                    CAPACITY  ACCESSMODES  STORAGECLASS         AGE" +
+                    "c21584cd-666c-42de-9df7-d72b7bae5aae  Bound   pvc-d4eb67d2-da83-11e7-bec9-5254002cd33f  1Gi       RWO          managed-nfs-storage  1m" +
+                    "" +
+                    "==> v1/Service" +
+                    "NAME                                                 CLUSTER-IP    EXTERNAL-IP  PORT(S)  AGE" +
+                    "c21584cd-666c-42de-9df7-d72b7bae5aae-nmaas-oxidized  10.13.82.246  <none>       80/TCP   1m" +
+                    "" +
+                    "==> v1beta1/Deployment" +
+                    "NAME                                                 DESIRED  CURRENT  UP-TO-DATE  AVAILABLE  AGE" +
+                    "c21584cd-666c-42de-9df7-d72b7bae5aae-nmaas-oxidized  1        1        1           1          1m" +
+                    "";
 
     private static final String EXAMPLE_HELM_STATUS_FOR_V3_COMMAND_OUTPUT =
             "NAME: testbastion\n" +
-            "LAST DEPLOYED: Thu Dec 31 11:25:34 2020\n" +
-            "NAMESPACE: default\n" +
-            "STATUS: deployed\n" +
-            "REVISION: 1\n" +
-            "TEST SUITE: None\n" +
-            "NOTES:\n" +
-            "1. Deployed.";
+                    "LAST DEPLOYED: Thu Dec 31 11:25:34 2020\n" +
+                    "NAMESPACE: default\n" +
+                    "STATUS: deployed\n" +
+                    "REVISION: 1\n" +
+                    "TEST SUITE: None\n" +
+                    "NOTES:\n" +
+                    "1. Deployed.";
 
     @Test
-    public void shouldReturnDeployedStatusFromInputString() {
+    void shouldReturnDeployedStatusFromInputString() {
         HelmCommandExecutor executor = new HelmCommandExecutor();
-        executor.helmVersion = HelmCommand.HELM_VERSION_2;
+        executor.setHelmVersion(HelmCommand.HELM_VERSION_2);
         assertThat(executor.parseStatus(EXAMPLE_HELM_STATUS_COMMAND_OUTPUT), equalTo(HelmPackageStatus.DEPLOYED));
     }
 
     @Test
-    public void shouldReturnDeployedStatusV3FromInputString() {
+    void shouldReturnDeployedStatusV3FromInputString() {
         HelmCommandExecutor executor = new HelmCommandExecutor();
-        executor.helmVersion = HelmCommand.HELM_VERSION_3;
+        executor.setHelmVersion(HelmCommand.HELM_VERSION_3);
         assertThat(executor.parseStatus(EXAMPLE_HELM_STATUS_FOR_V3_COMMAND_OUTPUT), equalTo(HelmPackageStatus.DEPLOYED));
     }
 
     @Test
-    public void shouldReturnUnknownStatusFromInputString() {
+    void shouldReturnUnknownStatusFromInputString() {
         HelmCommandExecutor executor = new HelmCommandExecutor();
         assertThat(executor.parseStatus("this is some example string"), equalTo(HelmPackageStatus.UNKNOWN));
-        executor.helmVersion = HelmCommand.HELM_VERSION_2;
+        executor.setHelmVersion(HelmCommand.HELM_VERSION_2);
         assertThat(executor.parseStatus("this is some example string"), equalTo(HelmPackageStatus.UNKNOWN));
-        executor.helmVersion = HelmCommand.HELM_VERSION_3;
+        executor.setHelmVersion(HelmCommand.HELM_VERSION_3);
         assertThat(executor.parseStatus("this is some example string"), equalTo(HelmPackageStatus.UNKNOWN));
     }
 
     @Test
-    public void shouldConstructChartName() {
+    void shouldConstructChartName() {
         HelmCommandExecutor executor = new HelmCommandExecutor();
-        executor.helmRepositoryName = "nmaas";
+        executor.setHelmRepositoryName("nmaas");
         assertThat(executor.constructChartNameWithRepo("chart-name"), equalTo("nmaas/chart-name"));
         assertThat(executor.constructChartNameWithRepo("repo-name/chart-name"), equalTo("repo-name/chart-name"));
     }
