64d20f9cae21895ebb3ddaa3bbd160a91bda68ba
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-05-08 15:13:39 +0200

    update sql migration

--- src/main/resources/db/migration/common/V1.8.0_20250508_1300__AddStateToCluster.sql
+++ src/main/resources/db/migration/common/V1.8.0_20250508_1300__AddStateToCluster.sql
@@ -1,2 +1,6 @@
 alter table k_cluster ADD COLUMN state varchar(255) DEFAULT 'UNKNOWN';
+alter table k_cluster ADD COLUMN current_state_since timestamp(6) with time zone not null;
+
+ALTER TABLE configuration ADD COLUMN health_check_job_cron varchar(255);
+
 
