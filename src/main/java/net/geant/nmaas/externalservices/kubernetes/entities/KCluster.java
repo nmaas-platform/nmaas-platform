24d2b0808409cc1c5e2fa11144bc470d7766940f
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-05-13 14:21:51 +0200

    fix using kubernetes request

--- src/main/java/net/geant/nmaas/externalservices/kubernetes/entities/KCluster.java
+++ src/main/java/net/geant/nmaas/externalservices/kubernetes/entities/KCluster.java
@@ -52,7 +52,7 @@ public class KCluster {
     @Column(nullable = false)
     private OffsetDateTime modificationDate;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 6000)
     private String clusterConfigFile;
 
     @Column(nullable = false)
