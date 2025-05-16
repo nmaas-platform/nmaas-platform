a1a6b8504c2f2993e5ec6c8cf5d144255f301999
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-05-16 14:16:23 +0200

    update logs for testings

--- src/main/java/net/geant/nmaas/externalservices/kubernetes/ClusterMonitoringJob.java
+++ src/main/java/net/geant/nmaas/externalservices/kubernetes/ClusterMonitoringJob.java
@@ -16,9 +16,9 @@ public class ClusterMonitoringJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        log.error("Triggering cluster health check...");
+        log.info("Triggering cluster health check...");
         remoteClusterManager.restoreFileIfMissing();
-        log.warn("File checked, everything looks fine. Next time: Update clusters state.");
+        log.info("File checked, everything looks fine. Next stage: Update clusters state.");
         remoteClusterManager.updateAllClusterState();
     }
 }
