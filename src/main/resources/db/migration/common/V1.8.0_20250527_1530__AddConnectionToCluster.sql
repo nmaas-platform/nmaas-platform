500706f201aa3afa239da4694e875eb752c3c7fe
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-05-27 16:15:36 +0200

    fix migration

--- src/main/resources/db/migration/common/V1.8.0_20250527_1530__AddConnectionToCluster.sql
+++ src/main/resources/db/migration/common/V1.8.0_20250527_1530__AddConnectionToCluster.sql
@@ -3,8 +3,8 @@ ADD COLUMN kcluster_id integer;
 
 ALTER TABLE kubernetes_nm_service_info
 ADD CONSTRAINT fk_kubernetes_nm_service_info_kcluster
-FOREIGN KEY (kcluster_id)
-REFERENCES kcluster (id)
+FOREIGN KEY (k_cluster_id)
+REFERENCES k_cluster (id)
 ON DELETE SET NULL;
 
 ALTER TABLE app_deployment
