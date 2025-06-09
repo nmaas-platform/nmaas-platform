7b7a00551f1d7ca9b1a259dd25d16260ba00610b
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-06-09 12:21:53 +0200

    return false  when id is null

--- src/main/java/net/geant/nmaas/externalservices/kubernetes/RemoteClusterManager.java
+++ src/main/java/net/geant/nmaas/externalservices/kubernetes/RemoteClusterManager.java
@@ -8,6 +8,7 @@ import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.geant.nmaas.externalservices.kubernetes.api.model.KClusterView;
 import net.geant.nmaas.externalservices.kubernetes.api.model.RemoteClusterView;
 import net.geant.nmaas.externalservices.kubernetes.entities.KCluster;
 import net.geant.nmaas.externalservices.kubernetes.entities.KClusterDeployment;
@@ -20,6 +21,7 @@ import net.geant.nmaas.notifications.templates.MailType;
 import net.geant.nmaas.portal.api.domain.UserView;
 import net.geant.nmaas.portal.persistent.entity.Domain;
 import net.geant.nmaas.portal.service.DomainService;
+import net.geant.nmaas.portal.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -37,8 +39,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -46,8 +48,8 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
+@Slf4j
 public class RemoteClusterManager implements ClusterMonitoringService {
 
     private final KClusterRepository clusterRepository;
@@ -55,6 +57,7 @@ public class RemoteClusterManager implements ClusterMonitoringService {
     private final KubernetesClusterDeploymentManager kClusterDeploymentManager;
     private final DomainService domainService;
     private final ApplicationEventPublisher eventPublisher;
+    private final UserService userService;
     private final ModelMapper modelMapper;
 
     public RemoteClusterView getClusterView(Long id) {
@@ -103,29 +106,41 @@ public class RemoteClusterManager implements ClusterMonitoringService {
         log.debug("Filed saved in: {}", savedPath);
         entity.setPathConfigFile(savedPath);
 
-        KCluster cluster = this.clusterRepository.save(entity);
-        log.debug("Cluster saved: {}", cluster.toString());
+        KCluster cluster = clusterRepository.save(entity);
+        log.debug("Cluster saved: {}", cluster);
         sendMail(cluster, MailType.REMOTE_CLUSTER_WELCOME_SUPPORT);
         return toView(cluster);
     }
 
-    public RemoteClusterView readClusterFile(RemoteClusterView view, MultipartFile file) {
-        checkRequest(view);
+    private void checkRequest(KCluster entity) {
+        if (entity.getName() == null) {
+            throw new IllegalArgumentException("Name of the cluster is null");
+        }
+        if (entity.getCodename() == null) {
+            throw new IllegalArgumentException("Codename of the cluster is null");
+        }
+        if (entity.getDescription() == null) {
+            throw new IllegalArgumentException("Description of the cluster is null");
+        }
+    }
+
+    public RemoteClusterView mapFile(RemoteClusterView view, MultipartFile file) {
+        checkRequestRead(view);
 
         ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
 
         try {
             ClusterConfigView configView = yamlMapper.readValue(file.getInputStream(), ClusterConfigView.class);
 
-            log.error("Mapped {}", configView.toString());
+            log.info("Mapped {}", configView.toString());
 
             if (configView.getClusters().isEmpty()) {
-                log.error("No clusters info provided in configuration file");
+                log.info("No clusters info provided in configuration file");
             } else if (configView.getClusters().size() == 1) {
-                log.error("One cluster provided, create view and return ");
+                log.info("One cluster provided, create view and return ");
                 KClusterDeployment deployment = modelMapper.map(kClusterDeploymentManager.getKClusterDeploymentView(), KClusterDeployment.class);
                 KClusterIngress ingress = modelMapper.map(kClusterIngressManager.getKClusterIngressView(), KClusterIngress.class);
-                return saveCluster(KCluster.builder()
+                return toView(KCluster.builder()
                                 .name(view.getName())
                                 .description(view.getDescription())
                                 .creationDate(OffsetDateTime.now())
@@ -137,23 +152,70 @@ public class RemoteClusterManager implements ClusterMonitoringService {
                                 .state(KClusterState.UNKNOWN)
                                 .contactEmail(view.getContactEmail())
                                 .currentStateSince(OffsetDateTime.now())
-                                .domains(!view.getDomainNames().isEmpty() ?  view.getDomainNames().stream().map(d -> {
-                                            Optional<Domain> dom = domainService.findDomain(d);
-                                            return dom.orElse(null);
-                                        }
-                                ).toList() : Collections.emptyList())
-                                .build(),
-                        file);
+                                .domains(prepareList(view))
+                                .build() );
 
             } else {
-                log.error("More than 1 cluster provided, not implemented yet");
+                log.info("More than 1 cluster provided, not implemented yet");
             }
 
-        } catch (IOException | NoSuchAlgorithmException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         return null;
+    }
+
+    public RemoteClusterView saveClusterFile(RemoteClusterView view, MultipartFile file) {
+        checkRequest(view);
+        try {
+                log.info("One cluster provided, create view and return ");
+                KClusterDeployment deployment;
+                KClusterIngress ingress;
+
+                if(view.getDeployment() != null) {
+                    deployment = modelMapper.map(view.getDeployment(), KClusterDeployment.class);
+                } else {
+                    deployment = modelMapper.map(kClusterDeploymentManager.getKClusterDeploymentView(), KClusterDeployment.class);
+                }
+
+                if(view.getIngress() != null) {
+                    ingress = modelMapper.map(view.getIngress(), KClusterIngress.class);
+                } else {
+                    ingress = modelMapper.map(kClusterDeploymentManager.getKClusterDeploymentView(), KClusterIngress.class);
+                }
+
+
+                return saveCluster(KCluster.builder()
+                                .name(view.getName())
+                                .description(view.getDescription())
+                                .creationDate(OffsetDateTime.now())
+                                .modificationDate(OffsetDateTime.now())
+                                .codename(view.getCodename())
+                                .clusterConfigFile(new String(file.getBytes()))
+                                .deployment(deployment)
+                                .ingress(ingress)
+                                .state(KClusterState.UNKNOWN)
+                                .contactEmail(view.getContactEmail())
+                                .currentStateSince(OffsetDateTime.now())
+                                .domains(prepareList(view))
+                                .build(),
+                        file);
+
+        } catch (IOException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Domain> prepareList(RemoteClusterView view) {
+        if (view == null || view.getDomainNames() == null) {
+            return Collections.emptyList();
+        }
+        return view.getDomainNames().stream().map(d -> {
+                    Optional<Domain> dom = domainService.findDomain(d);
+                    return dom.orElse(null);
+                }
+        ).toList();
     }
 
     public RemoteClusterView updateCluster(RemoteClusterView cluster, Long id) {
@@ -196,17 +258,15 @@ public class RemoteClusterManager implements ClusterMonitoringService {
         if (view.getDescription() == null) {
             throw new IllegalArgumentException("Description of the cluster is null");
         }
-    }
-
-    private void checkRequest(KCluster entity) {
-        if (entity.getName() == null) {
-            throw new IllegalArgumentException("Name of the cluster is null");
-        }
-        if (entity.getCodename() == null) {
+        if (view.getCodename() == null) {
             throw new IllegalArgumentException("Codename of the cluster is null");
         }
-        if (entity.getDescription() == null) {
-            throw new IllegalArgumentException("Description of the cluster is null");
+
+    }
+
+    private void checkRequestRead(RemoteClusterView view) {
+        if (view.getName() == null) {
+            throw new IllegalArgumentException("Name of the cluster is null");
         }
     }
 
@@ -234,15 +294,17 @@ public class RemoteClusterManager implements ClusterMonitoringService {
         }
     }
 
-    private RemoteClusterView toView(KCluster KCluster) {
-        RemoteClusterView view = modelMapper.map(KCluster, RemoteClusterView.class);
-        view.setDomainNames(KCluster.getDomains().stream().map(Domain::getName).toList());
+    private RemoteClusterView toView(KCluster kCluster) {
+        RemoteClusterView view = modelMapper.map(kCluster, RemoteClusterView.class);
+        if (Objects.nonNull(kCluster.getDomains())) {
+            view.setDomainNames(kCluster.getDomains().stream().map(Domain::getName).toList());
+        }
         return view;
     }
 
     @Override
     public void updateAllClusterState() {
-        restoreFileIfMissing();
+        restoreKubeconfigFileIfMissing();
         List<KCluster> kClusters = clusterRepository.findAll();
         kClusters.forEach(cluster -> {
             Config config = null;
@@ -260,8 +322,7 @@ public class RemoteClusterManager implements ClusterMonitoringService {
                 updateStateIfNeeded(cluster, KClusterState.UP);
 
             } catch (KubernetesClientException e) {
-                log.error("Can not connect to cluster {}", cluster.getCodename());
-                log.error(e.getMessage());
+                log.warn("Can't connect to cluster {} (message: {})", cluster.getCodename(), e.getMessage());
                 updateStateIfNeeded(cluster, KClusterState.DOWN);
             } catch (RuntimeException ex) {
                 log.error("Runtime error while checking health of cluster {}", ex.getMessage());
@@ -285,7 +346,13 @@ public class RemoteClusterManager implements ClusterMonitoringService {
     }
 
     private void sendMail(KCluster kCluster, MailType mailType) {
-        UserView recipient = UserView.builder().email(kCluster.getContactEmail()).username(kCluster.getContactEmail()).selectedLanguage("EN").build();
+        UserView recipient;
+        if (userService.existsByEmail(kCluster.getContactEmail())) {
+            recipient = modelMapper.map(userService.findByEmail(kCluster.getContactEmail()), UserView.class);
+        } else {
+            recipient = UserView.builder().email(kCluster.getContactEmail()).username(kCluster.getContactEmail()).selectedLanguage("EN").build();
+        }
+
         Map<String, Object> attr = new HashMap<>();
         attr.put("clusterId", kCluster.getId());
         attr.put("clusterCodename", kCluster.getCodename());
@@ -299,7 +366,7 @@ public class RemoteClusterManager implements ClusterMonitoringService {
         this.eventPublisher.publishEvent(new NotificationEvent(this, mailAttributes));
     }
 
-    public void restoreFileIfMissing() {
+    private void restoreKubeconfigFileIfMissing() {
         List<KCluster> clusters = clusterRepository.findAll();
         clusters.forEach(cluster -> {
             if (!isFileAvailable(cluster.getPathConfigFile())) {
@@ -318,9 +385,28 @@ public class RemoteClusterManager implements ClusterMonitoringService {
         });
     }
 
-    public boolean isFileAvailable(String pathStr) {
+    private boolean isFileAvailable(String pathStr) {
         Path path = Paths.get(pathStr);
         return Files.exists(path) && Files.isRegularFile(path) && Files.isReadable(path);
+    }
+
+    public void removeCluster(Long id) {
+        try {
+            if (clusterRepository.existsById(id)) {
+                this.clusterRepository.deleteById(id);
+            }
+        } catch (RuntimeException ex) {
+            log.warn("Can not delete cluster {}", id);
+            log.error("Exception: {}", ex.getMessage());
+        }
+
+    }
+
+    public boolean clusterExists(Long id) {
+        if(id == null) {
+            return false;
+        }
+        return clusterRepository.existsById(id);
     }
 
 }
