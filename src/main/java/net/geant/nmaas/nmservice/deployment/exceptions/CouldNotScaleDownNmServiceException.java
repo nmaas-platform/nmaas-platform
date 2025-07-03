a2234441f5e550e74d57c7d4594e22ee83753394
pkazimierowski <pkazimierowski@man.poznan.pl>
2025-07-03 09:47:06 +0200

    init exception

---
+++ src/main/java/net/geant/nmaas/nmservice/deployment/exceptions/CouldNotScaleDownNmServiceException.java
@@ -0,0 +1,8 @@
+package net.geant.nmaas.nmservice.deployment.exceptions;
+
+public class CouldNotScaleDownNmServiceException extends RuntimeException {
+
+    public CouldNotScaleDownNmServiceException(String message) {
+        super(message);
+    }
+}
