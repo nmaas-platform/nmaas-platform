4833dc6a89c6bfb6dbee141d731d29af6f989253
pkazimierowski <pkazimierowski@man.poznan.pl>
2026-04-02 08:38:04 +0200

    added pagination support to webhook history search endpoint

--- src/main/java/net/geant/nmaas/portal/service/WebhookHistoryService.java
+++ src/main/java/net/geant/nmaas/portal/service/WebhookHistoryService.java
@@ -3,19 +3,45 @@ package net.geant.nmaas.portal.service;
 import net.geant.nmaas.api.dto.webhooks.WebhookEventDto;
 import net.geant.nmaas.api.dto.webhooks.WebhookHistoryDto;
 import net.geant.nmaas.portal.persistence.entity.WebhookEventType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface WebhookHistoryService {
 
-    void create(WebhookEventDto webhook, Object payload, Integer responseStatus, String responseBody);
+    void create(
+            WebhookEventDto webhook,
+            Object payload,
+            Integer responseStatus,
+            String responseBody
+    );
 
     WebhookHistoryDto getById(Long id);
 
-    List<WebhookHistoryDto> search(Long webhookEventId, WebhookEventType eventType, String domainCodename,
-                                   LocalDateTime from, LocalDateTime to);
+    List<WebhookHistoryDto> search(
+            Long webhookEventId,
+            WebhookEventType eventType,
+            String domainCodename,
+            LocalDateTime from,
+            LocalDateTime to
+    );
 
-    List<WebhookHistoryDto> search(Long webhookEventId, WebhookEventType eventType, Long domainId,
-                                   LocalDateTime from, LocalDateTime to);
+    Page<WebhookHistoryDto> search(
+            Long webhookEventId,
+            WebhookEventType eventType,
+            String domainCodename,
+            LocalDateTime from,
+            LocalDateTime to,
+            Pageable pageable
+    );
+
+    List<WebhookHistoryDto> search(
+            Long webhookEventId,
+            WebhookEventType eventType,
+            Long domainId,
+            LocalDateTime from,
+            LocalDateTime to
+    );
 }
